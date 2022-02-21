fe8ebab461b868c7678b34232824a17e458fd8b0
BumgeunSong <isp1195@gmail.com>
2022-02-21 13:55:09 +0900

    Add setupCardView() with Stack view (not used)

--- PockerGameApp/PockerGameApp/ViewController.swift
+++ PockerGameApp/PockerGameApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupBackgroundPattern()
         setupCards()
+        // setupCardsWithStack()
     }
     
     func setupBackgroundPattern() {
@@ -38,6 +39,24 @@ class ViewController: UIViewController {
             cardView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
             cardView.widthAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.widthAnchor, multiplier: CGFloat(1.0/7.0)).isActive = true
             cardView.heightAnchor.constraint(equalTo: cardView.widthAnchor, multiplier: CGFloat(1.27)).isActive = true
+        }
+    }
+    
+    func setupCardsWithStack() {
+        let stackView = UIStackView()
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        
+        for _ in (0..<7) {
+            guard let cardBackImage = UIImage(named: "CardBackSide") else { continue }
+            let cardView = UIImageView(image: cardBackImage)
+            cardView.heightAnchor.constraint(equalTo: cardView.widthAnchor, multiplier: CGFloat(1.27)).isActive = true
+            stackView.addArrangedSubview(cardView)
         }
     }
 }
