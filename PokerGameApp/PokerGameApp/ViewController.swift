5f41ed3d7e4ea6876d8b42e435ff601599d6abe7
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-21 19:22:30 +0900

    UIImageView 7개 생성 및 일정 간격으로 표시
    - UIImageView 7개 생성
    - 너비:높이 1:1.27로 설정
    - 일정 간격으로 이미지 정렬

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  PokerGameApp
-//
-//  Created by 이준우 on 2022/02/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,11 +5,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackgroundColor()
+        setInitialImageView(7)
     }
     
     func setBackgroundColor(){
         self.view.backgroundColor = UIColor.init(patternImage: UIImage(named: "bg_pattern")!)
     }
-
+    
+    func setInitialImageView(_ cardsCount: CGFloat){
+        guard let image = UIImage(named: "card-back") else{ return }
+   
+        let cardMargin = CGFloat(10)
+        let cardWidth = (self.view.bounds.width - cardMargin*(cardsCount+1))/cardsCount
+        let cardHeight = cardWidth*CGFloat(1.27)
+        var cardXPosition = cardMargin
+        let cardYPosition = CGFloat(80)
+        
+        for _ in 0..<Int(cardsCount){
+            let imageView: UIImageView = UIImageView(frame: CGRect(x: cardXPosition, y: cardYPosition, width: cardWidth, height: cardHeight))
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            self.view.addSubview(imageView)
+            cardXPosition += cardMargin+cardWidth
+        }
+        
+    }
 }
 
