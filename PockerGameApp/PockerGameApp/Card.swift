310d27af38df147da6e99d96b48bf4ed7c0eff51
BumgeunSong <isp1195@gmail.com>
2022-02-21 18:28:02 +0900

    Make CardSuit,CardNumber Enum with CustomerStringConvertible

--- PockerGameApp/PockerGameApp/Card.swift
+++ PockerGameApp/PockerGameApp/Card.swift
@@ -7,38 +7,76 @@
 
 import Foundation
 
+// Named type을 선언할 때, 빠르고 불변성을 가진 Struct를 먼저 고려했습니다.
+// Class를 꼭 써야할 이유가 없으면 Struct를 쓰라고 배웠습니다.
+// Card 객체는 고유성이 필요한 객체가 아니고 (하트 6는 모두 똑같은 하트 6임)
+// 꼭 변경 가능한 상태를 공유해야 하는 상황도 아니기 때문에 Struct를 선택했습니다.
+
 struct Card: CustomStringConvertible {
     var suit: CardSuit
-    var number: Int? {
-        didSet { if !(1...13).contains(number!) { number = nil }}
-    }
+    var number: CardNumber
     
     var description: String {
-        var suitString = ""
-        switch suit {
-        case .spades: suitString = "♠️"
-        case .clubs: suitString = "♣️"
-        case .hearts: suitString = "♥️"
-        case .diamonds: suitString = "♦️"
-        }
-        
-        var numberString = ""
-        switch number {
-        case 1: numberString = "A"
-        case 11: numberString = "J"
-        case 12: numberString = "K"
-        case 13: numberString = "Q"
-        default: numberString = "\(number!)"
-        }
-        
-        return suitString + numberString
+        return suit.description + number.description
     }
 }
 
-enum CardSuit {
+// Spades와 ♠️이 매치된다거나, jack과 "J"가 매치된다는 정보는
+// Card 객체보다, CardSuit & CardNumber 객체와 관련성이 더 높다고 판단해 해당 객체에 추가했습니다.
+// Enum과 CustomStringCovertible를 사용했습니다.
+
+enum CardSuit: CustomStringConvertible {
     case spades
     case clubs
     case hearts
     case diamonds
+    
+    var description: String {
+        switch self {
+        case .spades: return "♠️"
+        case .clubs: return "♣️"
+        case .hearts: return "♥️"
+        case .diamonds: return "♦️"
+        }
+    }
 }
 
+// 처음에는 CardNumver를 Int로 선언하려고 했습니다.
+// 하지만 유효 범위의 지정과 Description 설정을 위해 Enum 타입으로 선언했습니다.
+// Enum의 case명에는 숫자를 쓸 수 없습니다.
+// 따라서 각 CardNumber가 가지는 숫자값은 RawValue(Int)로 담아주었습니다.
+// 이후 RawValue로 enum을 생성하거나, 게임 로직에서 rawValue를 호출하여 사용할 수 있을 거 같습니다.
+
+enum CardNumber: Int, CustomStringConvertible {
+    case ace = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+    case nine = 9
+    case ten = 10
+    case jack = 11
+    case queen = 12
+    case king = 13
+    
+    var description: String {
+        switch self {
+        case .ace: return "A"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+        case .ten: return "10"
+        case .jack: return "J"
+        case .queen: return "Q"
+        case .king: return "K"
+        }
+    }
+}
