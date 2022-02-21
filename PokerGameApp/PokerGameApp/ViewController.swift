aaf60e4297367631a2dc8371db86bafdabb5c9c3
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-21 18:52:14 +0900

    [Feat] cardImageView 7개 생성 및 추가

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,20 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setBackgroundImage()
+        setCardImageViews()
+    }
+    
+    let cardCountPerRow = 7
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func setBackgroundImage() {
         guard let backgroundImage = UIImage(named: "bg_pattern") else { return }
         self.view.backgroundColor = UIColor(patternImage: backgroundImage)
     }
     
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-        
+    func getViewWidth() -> CGFloat {
+        let bounds: CGRect = UIScreen.main.bounds
+        return bounds.width
     }
     
-
+    func createCardImageView() -> UIImageView {
+        let cardImageView = UIImageView()
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.contentMode = .scaleAspectFit
+        cardImageView.image = UIImage(named: "card-back")
+        
+        return cardImageView
+    }
+    
+    func setCardImageViews() {
+        for count in 0..<cardCountPerRow {
+            let card = createCardImageView()
+            
+            let cardImageViewWidth = getViewWidth() / CGFloat(cardCountPerRow)
+            let cardImageViewHeight = cardImageViewWidth * 1.27
+            let cardImageViewY =  CGFloat(50)
+            let cardImageViewX = (CGFloat(count) * cardImageViewWidth)
+            
+            card.frame = CGRect(x: cardImageViewX, y: cardImageViewY, width: cardImageViewWidth, height: cardImageViewHeight)
+            
+            view.addSubview(card)
+        }
+    }
 }
 
