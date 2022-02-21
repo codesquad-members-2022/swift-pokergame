42065966258aa1298b951cb7add40e7de2fb61cc
sol <hskim6521@naver.com>
2022-02-21 15:49:33 +0900

    Feat: SwiftUI의 ViewPreview 구조체 선언
    코드로 View 생성한걸 Project Run하지않고 바로 확인하기 위해 SwiftUI import하고 ViewPreview 구조체 선언해줌

--- PockerGameApp/PockerGameApp/ViewController.swift
+++ PockerGameApp/PockerGameApp/ViewController.swift
@@ -17,3 +17,24 @@ class ViewController: UIViewController {
 
 }
 
+import SwiftUI
+
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        return ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct ViewPreview: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+            .previewLayout(.sizeThatFits)
+            .previewDevice("iPhone 13 Pro")
+    }
+}
