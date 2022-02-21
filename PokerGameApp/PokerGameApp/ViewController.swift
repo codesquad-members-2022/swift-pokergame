fe84e6595a23945c23be9cce7e884ff3d3fea0c8
sanghee <anshee97@naver.com>
2022-02-21 17:12:07 +0900

    Add: Add 7 UIImageViews

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cards = [UIImageView]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
+        
+        for i in 0..<7 {
+            cards.append(UIImageView(image: UIImage(named: "card-back")))
+        }
+        print(cards)
     }
 }
 
