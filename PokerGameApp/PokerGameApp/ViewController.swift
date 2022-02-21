359b085cebc77f61de4e86597d6adb3cec660db4
sungju-kim <wnwn0719@gmail.com>
2022-02-21 18:47:27 +0900

    Refactor 반복적으로 사용하는 숫자값 변수 선언

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -13,16 +13,18 @@ class ViewController: UIViewController {
         if let backGround = UIImage(named: "bg_pattern.png"){
             self.view.backgroundColor = UIColor(patternImage: backGround)
         }
-        createCardSet()
+        createCardSet(numberOfCard: 7)
     }
-    func createCardSet(){
+    func createCardSet(numberOfCard: CGFloat){
         let screenSize = UIScreen.main.bounds
-        let cardWidth = (screenSize.width - 32) / 7
+        let blank : CGFloat = 4
+        let totalMargin : CGFloat = (numberOfCard + 1) * blank
+        let cardWidth = (screenSize.width - totalMargin) / numberOfCard
         let cardHeight = cardWidth * 1.27
         let cardBackSide = UIImage(named: "card-back.png")
         let margin : CGFloat = 55
-        for i in 0..<7 {
-            let imageView = UIImageView(frame: CGRect(x: (margin * CGFloat(i))+4, y: 40, width: cardWidth,height: cardHeight))
+        for i in 0..<Int(numberOfCard) {
+            let imageView = UIImageView(frame: CGRect(x: (margin * CGFloat(i))+blank, y: 40, width: cardWidth,height: cardHeight))
             imageView.contentMode = .scaleToFill
             imageView.layer.cornerRadius = 3
             imageView.clipsToBounds = true
