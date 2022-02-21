bf9bf058519b2851bf0249da04d789a832e59b44
shingha <shingha1124@gmail.com>
2022-02-21 11:03:36 +0900

    [Style] 배경 패턴 적용 및 카드 뷰의 영역 체크

--- PokerGame/PokerGame/Sources/Present/PokerBoardViewController.swift
+++ PokerGame/PokerGame/Sources/Present/PokerBoardViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PokerBoardViewController: UIViewController {
     
+    let cardContainerView = UIView()
+    let cards: [UIImageView] = Array(repeating: UIImageView(), count: 7)
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         .lightContent
     }
@@ -26,10 +29,24 @@ class PokerBoardViewController: UIViewController {
     }
     
     private func attribute() {
-        
+        if let backImage = UIImage(named: "bg_pattern") {
+            self.view.backgroundColor = UIColor(patternImage: backImage)
+        }
+        cardContainerView.backgroundColor = .red
     }
     
     private func layout() {
+        self.view.addSubview(cardContainerView)
+        cards.forEach {
+            cardContainerView.addSubview($0)
+        }
         
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        cardContainerView.translatesAutoresizingMaskIntoConstraints = false
+        cardContainerView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        cardContainerView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        cardContainerView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        cardContainerView.heightAnchor.constraint(equalToConstant: 80).isActive = true
     }
 }
