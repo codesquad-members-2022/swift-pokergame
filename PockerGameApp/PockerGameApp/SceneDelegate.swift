d8c6622640bc22a03966c62a9413d9596e410861
sol <hskim6521@naver.com>
2022-02-21 11:12:11 +0900

    Docs: 프로젝트 파일 생성

---
+++ PockerGameApp/PockerGameApp/SceneDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  SceneDelegate.swift
+//  PockerGameApp
+//
+//  Created by 김한솔 on 2022/02/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
---
+++ PockerGameApp/PockerGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PockerGameApp
+//
+//  Created by 김한솔 on 2022/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
