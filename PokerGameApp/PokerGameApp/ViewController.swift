838f52ee9eef3a00ae9c642c642966cb14a0953f
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-21 22:19:41 +0900

    [ADD]Cards display functionality @ViewController.viewDidAppear

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cards: [UIImageView]!
+    let spaceBetweenCards :CGFloat = 8
+    let screenSize: CGRect = UIScreen.main.bounds
+    var safeArea : UIEdgeInsets!
+    var cardWidth : CGFloat {
+        (screenSize.width - (spaceBetweenCards * CGFloat(cards.count) - 1))/CGFloat(cards.count)
+    }
+    var startOffsetX:CGFloat = 4
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"bg_pattern.png")!)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        safeArea = self.view.safeAreaInsets
+        for (index,card) in cards.enumerated() {
+            card.image = UIImage(named: "card-back.png")
+            card.frame = CGRect(x: startOffsetX + CGFloat(index) * (cardWidth + spaceBetweenCards), y: safeArea.top, width: cardWidth, height: cardWidth * 1.26)
+        }
+    }
+        
 }
 
