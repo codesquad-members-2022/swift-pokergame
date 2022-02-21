6c932b2f18c759518c70d6406f4f92ab566f7598
sanghee <anshee97@naver.com>
2022-02-21 17:21:28 +0900

    Feat: Set 7 UIImageview's image size and add it to subview

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     var cards = [UIImageView]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,7 +20,19 @@ class ViewController: UIViewController {
         for i in 0..<7 {
             cards.append(UIImageView(image: UIImage(named: "card-back")))
         }
-        print(cards)
+        
+        // 화면 크기
+        print(self.view.frame.width, self.view.frame.height)
+        let width = self.view.frame.width / 7
+        let height = width * 1.27
+        
+        for i in 0..<7 {
+            cards[i].frame.size.width = width
+            cards[i].frame.size.height = height
+            view.addSubview(cards[i])
+        }
+        
+        print("추가된 이미지뷰의 수", view.subviews.count)
     }
 }
 
