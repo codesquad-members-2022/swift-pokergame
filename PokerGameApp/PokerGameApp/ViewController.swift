06377f4658fac476d8fe37547669182dcd06a180
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-22 00:03:59 +0900

    [Refactor] PR 리뷰받은 부분 수정 - createCardImageView() 함수 호출할 때 마다 UIImage 객체가 불필요하게 반복 생성되는 것 수정 - setCardImageViews() 함수의 반복문 내 불필요한 중복 수정
    (cherry picked from commit a0fa06ba5cf149e79e5ee5be5f31c0151c19b67d)

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     }
     
     let cardCountPerRow = 7
+    let cardImage = UIImage(named: "card-back")
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -36,20 +37,19 @@ class ViewController: UIViewController {
         let cardImageView = UIImageView()
         cardImageView.translatesAutoresizingMaskIntoConstraints = false
         cardImageView.contentMode = .scaleAspectFit
-        cardImageView.image = UIImage(named: "card-back")
-        
+        cardImageView.image = cardImage
         return cardImageView
     }
     
     func setCardImageViews() {
         let margin: CGFloat = 10
         
+        let cardImageViewWidth = (getViewWidth()) / CGFloat(cardCountPerRow) - margin
+        let cardImageViewHeight = cardImageViewWidth * 1.27
+        let cardImageViewY =  CGFloat(50)
+        
         for count in 0..<cardCountPerRow {
             let card = createCardImageView()
-            
-            let cardImageViewWidth = (getViewWidth()) / CGFloat(cardCountPerRow) - margin
-            let cardImageViewHeight = cardImageViewWidth * 1.27
-            let cardImageViewY =  CGFloat(50)
             let cardImageViewX = (CGFloat(count) * (cardImageViewWidth + margin)) + margin / 2
             
             card.frame = CGRect(x: cardImageViewX, y: cardImageViewY, width: cardImageViewWidth, height: cardImageViewHeight)
