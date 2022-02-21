b92055ff3cadec513098b2a2ece27e84084c8d46
shingha <shingha1124@gmail.com>
2022-02-21 10:39:44 +0900

    [add] BoardViewController 생성 및 기본 구조 정의

---
+++ PokerGame/PokerGame/Sources/Present/PokerBoardViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PokerBoardViewController.swift
+//  PokerGame
+//
+//  Created by seongha shin on 2022/02/21.
+//
+
+import Foundation
+import UIKit
+
+class PokerBoardViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+        attribute()
+        layout()
+    }
+    
+    private func bind() {
+        
+    }
+    
+    private func attribute() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+}
