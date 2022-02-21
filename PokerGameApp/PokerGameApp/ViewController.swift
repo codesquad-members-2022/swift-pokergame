0116563175a78761c9f05f6848016bb64322a984
asqw887 <asqw887@gmail.com>
2022-02-21 18:00:41 +0900

    카드 뒷면 구현하기

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -12,8 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_patterns") ?? UIImage())
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern") ?? UIImage())
+        
+        configureCard()
                                             
+    }
+    
+    func configureCard(){
+        
+        let screenWidth = UIScreen.main.bounds.size.width
+        
+        for i in 0..<7 {
+            
+            let cardImage = UIImage(named: "card-back") ?? UIImage()
+            let cardLeftXPosition: CGFloat =  CGFloat(CGFloat(i) * (screenWidth/7.0) + 5.0 )
+        
+            let card = UIImageView(frame: CGRect(x: cardLeftXPosition , y: 70, width: screenWidth/7.0 - 5, height: (screenWidth/7.0 - 5) * 1.25))
+            
+            card.image = cardImage
+            card.contentMode = .scaleAspectFit
+      
+
+            self.view.addSubview(card)
+        }
+        
+      
+        
+        
+        
     }
 
 
