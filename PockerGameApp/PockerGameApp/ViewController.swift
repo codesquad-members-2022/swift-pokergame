65e172e6c813816565f77ebf32d3b2f0831ab648
BumgeunSong <isp1195@gmail.com>
2022-02-21 11:50:38 +0900

    Add background pattern

--- PockerGameApp/PockerGameApp/ViewController.swift
+++ PockerGameApp/PockerGameApp/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let patternImage = UIImage(named: "BackgroundPattern") {
+            self.view.backgroundColor = UIColor(patternImage: patternImage)
+        }
     }
 
 
