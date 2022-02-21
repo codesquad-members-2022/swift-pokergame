ca55cf8b73edf530eb5ae11fbb59d3748f8b16b2
shingha <shingha1124@gmail.com>
2022-02-21 20:38:31 +0900

    [테스트] 카드 배분 테스트에 필요한 코드 작성

--- PokerGame/PokerGameTests/PokerGameTests.swift
+++ PokerGame/PokerGameTests/PokerGameTests.swift
@@ -15,35 +15,56 @@ class PokerGameTests: XCTestCase {
     override func tearDownWithError() throws {
     }
     
+    func testPokerCardDistribution() {
+        let pokerType = PokerType.sevenCard
+        let pokerGame = PokerGame(playType: pokerType, playerCount: 3)
+        pokerGame.play()
+        
+        let player = pokerGame.players
+        player.forEach {
+            print(getPrint(player: $0))
+        }
+        
+        print(getPrint(player: pokerGame.dealer))
+        
+        player.forEach {
+            XCTAssertEqual($0.cards.count, pokerType.rawValue)
+        }
+        XCTAssertEqual(pokerGame.dealer.cards.count, pokerType.rawValue)
+    }
+    
+    func getPrint(player: Player) -> String {
+        let cards = player.cards.reduce(""){$0 + "\($1), "}.dropLast(2)
+        return "\(player.name)\t [\(cards)] "
+    }
+    
     func testCardDeckReset() {
         let removeCount = 3
         let cardDeck = CardDeck()
         (0..<removeCount).forEach { _ in
-            guard let card = cardDeck.removeOne() else {
-                print("카드를 못뽑았습니다.")
+            guard let _ = cardDeck.removeOne() else {
                 return
             }
-            print("뽑은카드: \(card)")
         }
         XCTAssertEqual(cardDeck.count, 52 - removeCount)
-        
-        print("카드 덱 리셋")
         cardDeck.reset()
         XCTAssertEqual(cardDeck.count, 52)
     }
     
     func testCardDeckRemoveOne() {
         let cardDeck = CardDeck()
-        var removeCard = [String]()
-        (0..<52).forEach { index in
+        var removeCards = [String]()
+        (0..<52).forEach { _ in
             guard let card = cardDeck.removeOne() else {
                 return
             }
+ 
+            let stringCard = card.toString()
+            print(stringCard)
             
-            XCTAssertFalse(removeCard.contains(card.description), "중복된 카드가 뽑혔습니다!")
-            removeCard.append(card.description)
+            XCTAssertFalse(removeCards.contains(stringCard), "중복된 카드가 뽑혔습니다")
+            removeCards.append(stringCard)
         }
-        XCTAssertEqual(cardDeck.count, 51)
     }
     
     func testCardDeckShuffle() {
@@ -59,7 +80,7 @@ class PokerGameTests: XCTestCase {
     }
     
     func testCardInstance() {
-        let cards = (0..<15).flatMap { number -> CardData? in
+        let cards = (0..<15).compactMap { number -> CardData? in
             guard number > 0 && number <= 13 else {
                 print("nil")
                 return nil
