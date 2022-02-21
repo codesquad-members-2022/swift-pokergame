79597f6d557f377aa5f39cb1394ae07a1d6287f9
Song TaeHwan <axoghks@gmail.com>
2022-02-21 11:22:19 +0900

    build: Initialize ios project

---
+++ TexasHoldem/TexasHoldem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TexasHoldem
+//
+//  Created by 송태환 on 2022/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
