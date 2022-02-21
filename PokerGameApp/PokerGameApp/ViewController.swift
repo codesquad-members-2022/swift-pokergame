381eea333ab7d8eed0a3e478ecaaac02cfc66145
Jinsujin <sylpid003@gmail.com>
2022-02-21 17:34:22 +0900

    Design: ViewController 배경 패턴 이미지 적용

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let pattern: UIImage = #imageLiteral(resourceName: "bg_pattern")
+        view.backgroundColor = UIColor(patternImage: pattern)
     }
 
 
