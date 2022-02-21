83c3f5969ef16c963bfe75bb8388c3d8ce8790e5
Damagucci-Juice <jsr9874@gmail.com>
2022-02-21 11:05:37 +0900

    Add : 프로젝트 생성

---
+++ PokerGameApp/PokerGameAppUITests/PokerGameAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PokerGameAppUITestsLaunchTests.swift
+//  PokerGameAppUITests
+//
+//  Created by YEONGJIN JANG on 2022/02/21.
+//
+
+import XCTest
+
+class PokerGameAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
