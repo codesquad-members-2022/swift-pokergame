5926d5a780a96e9da782da90d02904facce02593
Jinsujin <sylpid003@gmail.com>
2022-02-21 19:08:07 +0900

    feat: 카드 이미지 추가 함수 작성

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -14,8 +14,29 @@ class ViewController: UIViewController {
         
         let pattern: UIImage = #imageLiteral(resourceName: "bg_pattern")
         view.backgroundColor = UIColor(patternImage: pattern)
+        
+        addCardImages(superView: self.view, count: 7)
     }
 
-
+    private func addCardImages(superView: UIView, count: Int) {
+        let backgroundSize = CGSize(width: superView.bounds.width * 0.9, height: 100)
+        let backgroundPointX = (superView.bounds.width - backgroundSize.width) / 2
+        let background = UIView(frame: CGRect(origin: CGPoint(x: backgroundPointX, y: 100),
+                                size: backgroundSize))
+        background.backgroundColor = .brown
+        superView.addSubview(background)
+        
+        let spacing: CGFloat = 10
+        let cardWidth = (background.frame.size.width - spacing * CGFloat(count - 1)) / CGFloat(count)
+        let cardHeight = cardWidth * 1.27
+        for cardIndex in 0..<count {
+            let cardBack: UIImage = #imageLiteral(resourceName: "card-back")
+            let space = CGFloat(cardIndex == 0 ? 0 : spacing * CGFloat(cardIndex))
+            let x = CGFloat(cardIndex) * cardWidth + space
+            let cardImageView =  UIImageView(frame: CGRect(x: x, y: 0, width: cardWidth, height: cardHeight))
+            cardImageView.image = cardBack
+            background.addSubview(cardImageView)
+        }
+    }
 }
 
