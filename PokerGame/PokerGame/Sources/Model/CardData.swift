23f674363b1a7bd3d5e856d446ecd392255b1c35
shingha <shingha1124@gmail.com>
2022-02-21 17:46:01 +0900

    [feature] 덱의 shuffle, removeOne, reset, count의 기능 구현

--- PokerGame/PokerGame/Sources/Model/CardData.swift
+++ PokerGame/PokerGame/Sources/Model/CardData.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+///카드정보를 담고 있는 구조체
+/// struct 만든 이유는 카드데이터의 경우 패턴과 숫자의 값만 가지고 있고,
+/// 이 값들은 외부에서 변환하거나, 수정이 이루어질 필요가 없는 데이터
+/// 초기화 시 설정된 값만 사용한다
 struct CardData {
     let pattern: CardPattern
     let number: Int
