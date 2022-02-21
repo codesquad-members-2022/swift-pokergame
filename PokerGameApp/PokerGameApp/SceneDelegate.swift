350a5fc96e14263d29d0c9fa2d8320d4d00bca94
sanghee <anshee97@naver.com>
2022-02-21 10:25:52 +0900

    Initial commit

---
+++ PokerGameApp/PokerGameApp/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  PokerGameApp
+//
+//  Created by Selina on 2022/02/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
