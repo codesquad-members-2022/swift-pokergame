7403c583b50ca550cbefbd6a3d712524831ea893
Jihee hwang <hwangjihee94@gmail.com>
2022-02-21 18:37:51 +0900

    Fix : App Name

---
+++ PokerGame/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  PokerGame
+//
+//  Created by Jihee hwang on 2022/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBackgroundImage()
+        addCard(count: 7)
+    }
+    
+    private func setBackgroundImage() {
+        // Color에 Image넣어야해서 옵셔널바인딩
+        guard let backgroundImage = UIImage(named: "bg_pattern.png") else { return }
+        view.backgroundColor = UIColor(patternImage: backgroundImage)
+    }
+    
+    private func addCard(count: Int) {
+        let card = "card-back.png"
+        let width = view.bounds.width / Double(count)
+        let hight = width * 1.27
+        var positionX = 0.0
+        
+        for addImage in 0..<count {
+            let image = UIImage(named: card)
+            let imageView = UIImageView(image: image)
+            // 여백을 위해 width - 3
+            imageView.frame = CGRect(x: positionX, y: 50, width: width - 3, height: hight)
+            
+            positionX += width
+            view.addSubview(imageView)
+        }
+        
+    }
+    
+}
