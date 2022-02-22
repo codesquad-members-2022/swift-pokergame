4e15ea8e594a1439aa2fe2cc3333f438ee764c8b
BumgeunSong <isp1195@gmail.com>
2022-02-22 15:38:59 +0900

    Create CardDeck struct skeleton

---
+++ PockerGameApp/PockerGameApp/CardDeck.swift
@@ -0,0 +1,32 @@
+//
+//  CardDeck.swift
+//  PokerGameApp
+//
+//  Created by Bumgeun Song on 2022/02/22.
+//
+
+import Foundation
+
+struct CardDeck {
+    private var cards: [Card]
+    
+    init() {
+        
+    }
+    
+    var count: Int {
+        return cards.count
+    }
+    
+    func shuffle() {
+        
+    }
+    
+    func removeOne() {
+        
+    }
+    
+    func reset() {
+        
+    }
+}
