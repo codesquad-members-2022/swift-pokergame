d4d6ecd42c19609e79ff781a36d1cf4f4b980fd0
asqw887 <asqw887@gmail.com>
2022-02-23 00:36:57 +0900

    refector - Card Class 리팩토링

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -10,18 +10,30 @@ import Foundation
 
 class Card: CustomStringConvertible{
     
-    
     // 카드 모양은 한정되어 있으므로 enum으로 구현
-    enum symbol: String {
-        case heart = "♥️"
-        case diamond = "♦️"
-        case club = "♣️"
-        case spade = "♠️"
+    enum Symbol: String, CustomStringConvertible {
+        case heart
+        case diamond
+        case club
+        case spade
+        
+        var description: String {
+            switch(self){
+            case .heart :
+                return "♥️"
+            case .diamond :
+                return "♦️"
+            case .club :
+                return "♣️"
+            case .spade :
+                return "♠️"
+            }
+        }
     }
     
     // Card의 rank는 한정되어있고, 후에 카드 랭크를 비교할때는 rawValue로 처리를 해주면
     // 편할것 같아 enum으로 구현.
-    enum rank: Int, CustomStringConvertible {
+    enum Rank: Int, CustomStringConvertible {
         case A = 1
         case two
         case three
@@ -37,34 +49,32 @@ class Card: CustomStringConvertible{
         case K
         
         var description: String {
-            
             switch(self){
-            case .A : return "A"
-            case .J : return "J"
-            case .Q : return "Q"
-            case .K : return "K"
-            default : return String(describing: self.rawValue)
+            case .A :
+                return "A"
+            case .J :
+                return "J"
+            case .Q :
+                return "Q"
+            case .K :
+                return "K"
+            default :
+                return String(describing: self.rawValue)
             }
         }
     }
      
+
+    let symbol: Symbol
+    let number: Rank
     
-    
-    let symbol: symbol
-    let number: rank
-    
-    init(symbol: symbol, rank: rank){
-        
+    init(symbol: Symbol, rank: Rank){
         self.symbol = symbol
         self.number = rank
-        
-        
     }
     
 
     var description: String {
-        return "\(self.symbol.rawValue)\(self.number.description)"
+        return "\(self.symbol.rawValue)\(self.number)"
     }
-
-    
 }
