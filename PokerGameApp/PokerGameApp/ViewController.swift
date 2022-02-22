6f4ab8ed72989eac0a8043bdba60bc077baceafb
OceanShape <oceanshape700@gmail.com>
2022-02-22 16:53:22 +0900

    feat: Add card-back UIImageView

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let cardWidth = screenWidth / 7
+        
+        for i in 0..<7 {
+            let imageView = UIImageView()
+
+            imageView.backgroundColor = UIColor(patternImage: UIImage(named: "card-back.png")!)
+            imageView.contentMode = .scaleToFill
+            imageView.image = UIImage(named: "card-back.png")!
+            imageView.frame = CGRect(x: CGFloat(i) * cardWidth, y: 35, width: cardWidth, height: cardWidth * 1.27)
+            
+            self.view.addSubview(imageView)
+        }
+        
     }
 
 
