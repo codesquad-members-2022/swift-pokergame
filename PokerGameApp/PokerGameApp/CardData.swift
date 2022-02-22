d2a9f1fb83310cf89fd5f33da64c4c86fe4442f0
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-22 16:16:11 +0900

    (Step2)[Feat] 코드에 대한 설명 주석 추가

--- PokerGameApp/PokerGameApp/CardData.swift
+++ PokerGameApp/PokerGameApp/CardData.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
+//카드의 값을 표현하는 열거형
 enum CardData {
+    //카드의 값 중 모양을 표현하는 열거형
     enum FaceData: String {
         case spade
         case club
@@ -15,6 +17,7 @@ enum CardData {
         case diamond
     }
 
+    //카드의 값 중 숫자를 표현하는 열거형
     enum NumericData: Int {
         case ace = 1
         case two
@@ -32,6 +35,9 @@ enum CardData {
     }
 }
 
+//CustomStringConvertible을 채택하고 description 프로퍼티를 구현해서
+//다른 객체가 해당 열거형의 rawValue에 직접 접근하는 것을 막고,
+//각 열거형의 case마다 어떤 String값을 반환할지 지정해줄 수 있습니다.
 extension CardData.FaceData: CustomStringConvertible {
     var description: String {
         switch self {
