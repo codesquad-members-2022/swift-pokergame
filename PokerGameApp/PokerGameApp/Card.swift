60527cb47fe9f9f99f8437b03d979a617cf48461
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-22 15:56:18 +0900

    (Step2)[Feat] Card 구조체 타입 구현

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
-class Card {
+struct Card {
+    private let faceData: CardData.FaceData
+    private let numericData: CardData.NumericData
     
+    init(faceData: CardData.FaceData, numericData: CardData.NumericData) {
+        self.faceData = faceData
+        self.numericData = numericData
+    }
+}
+
+extension Card: CustomStringConvertible {
+    var description: String {
+        return "\(self.faceData)\(self.numericData)"
+    }
 }
