8baa2ab6e926201634a80f4925d4f0f091ec20ae
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-22 12:02:27 +0900

    [Refactor] ImageView와 StackView 생성 및 출력 관련 코드들 함수화
    (cherry picked from commit c16f83a511b14b9b9ae60967264dc5e4fde7962c)

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -12,49 +12,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setBackgroundImage()
-        setCardImageViews()
+        setUpBackgroundImage()
+        addCardImageViewIntoCardStackView(repeat: 7)
     }
-    
-    let cardCountPerRow = 7
-    let cardImage = UIImage(named: "card-back")
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-    func setBackgroundImage() {
+    private func setUpBackgroundImage() {
         guard let backgroundImage = UIImage(named: "bg_pattern") else { return }
         self.view.backgroundColor = UIColor(patternImage: backgroundImage)
     }
     
-    func getViewWidth() -> CGFloat {
-        let bounds: CGRect = UIScreen.main.bounds
-        return bounds.width
+    private func createCardImageView(cardImage: UIImage) -> UIImageView {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = cardImage
+        return imageView
     }
     
-    func createCardImageView() -> UIImageView {
-        let cardImageView = UIImageView()
-        cardImageView.translatesAutoresizingMaskIntoConstraints = false
-        cardImageView.contentMode = .scaleAspectFit
-        cardImageView.image = cardImage
-        return cardImageView
+    private func createCardStackView() -> UIStackView {
+        let cardStackView = UIStackView()
+        cardStackView.translatesAutoresizingMaskIntoConstraints = false
+        cardStackView.axis = .horizontal
+        cardStackView.distribution = .fillEqually
+        cardStackView.spacing = 5
+        return cardStackView
     }
     
-    func setCardImageViews() {
-        let margin: CGFloat = 10
+    private func setUpCardStackView(cardStackView: UIStackView) {
+        self.view.addSubview(cardStackView)
+        cardStackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        cardStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        cardStackView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.95).isActive = true
+        cardStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
+    }
+    
+    private func addCardImageViewIntoCardStackView(repeat count: Int = 1) {
+        guard let cardImage = UIImage(named: "card-back") else { return }
+        let cardStackView = createCardStackView()
+        setUpCardStackView(cardStackView: cardStackView)
         
-        let cardImageViewWidth = (getViewWidth()) / CGFloat(cardCountPerRow) - margin
-        let cardImageViewHeight = cardImageViewWidth * 1.27
-        let cardImageViewY =  CGFloat(50)
-        
-        for count in 0..<cardCountPerRow {
-            let card = createCardImageView()
-            let cardImageViewX = (CGFloat(count) * (cardImageViewWidth + margin)) + margin / 2
-            
-            card.frame = CGRect(x: cardImageViewX, y: cardImageViewY, width: cardImageViewWidth, height: cardImageViewHeight)
-            
-            view.addSubview(card)
+        for _ in 0..<count {
+            let cardImageView = createCardImageView(cardImage: cardImage)
+            cardStackView.addArrangedSubview(cardImageView)
         }
     }
 }
