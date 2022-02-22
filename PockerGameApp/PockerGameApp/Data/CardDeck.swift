ee4ec44f9b4903d6b854c321b427b017d8eeb735
wnsxor1993 <wnsxor1993@naver.com>
2022-02-22 16:24:43 +0900

    Chore: Card 클래스 init 수정 외 부가 요소 수정 - 범용성이 떨어지던 Card 클래스 init 수정 - CardNumber 타입 enum으로 수정 - makeCardInfo 함수를 makeRandomCardInfo로 이름 변경 후, 랜덤 요소를 적절한 값으로 변환해서 Card에 전달하는 로직으로 수정 - Card 인스턴스 생성에 실패하여 nil이 할당되면 alert 출력 추가

---
+++ PockerGameApp/PockerGameApp/Data/CardDeck.swift
@@ -0,0 +1,13 @@
+//
+//  CardDeck.swift
+//  PockerGameApp
+//
+//  Created by juntaek.oh on 2022/02/22.
+//
+
+import Foundation
+
+// CardDeck은 각각의 고유 인스턴스인 Card들을 담는 단순한 통 개념이므로 Struct로 구현
+struct CardDeck{
+    private var deck: [Card] = []
+}
