e2ce89caf7959266d4c2f3615e0e6a21bff39dc1
sanghee <anshee97@naver.com>
2022-02-22 14:46:45 +0900

    Feat: Add CardShape Enumeration

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -10,3 +10,14 @@ import Foundation
 struct Card {
     
 }
+
+
+
+// 카드 모양은 4종류이기 때문이 아래와 같이 선언했습니다.
+// rawValue의 자료형을 String으로 선언하면 rawValue에 원시값을 일일히 저장하지 않아도 case 이름과 동일한 문자열이 rawValue로 생성되기 때문에 아래와 같이 선언했습니다.
+enum CardShape: String {
+    case spade
+    case diamond
+    case heart
+    case club
+}
