2c2ed6f3a199ae299ac997defbd6d454c2ed17a1
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-22 15:54:21 +0900

    (Step2)[Feat] CardData 열거형 타입 구현

--- PokerGameApp/PokerGameApp/CardData.swift
+++ PokerGameApp/PokerGameApp/CardData.swift
@@ -8,5 +8,58 @@
 import Foundation
 
 enum CardData {
-    
+    enum FaceData: String {
+        case spade
+        case club
+        case heart
+        case diamond
+    }
+
+    enum NumericData: Int {
+        case ace = 1
+        case two
+        case three
+        case four
+        case five
+        case six
+        case seven
+        case eight
+        case nine
+        case ten
+        case jack
+        case queen
+        case king
+    }
+}
+
+extension CardData.FaceData: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .spade:
+            return "♠"
+        case .club:
+            return "♣"
+        case .heart:
+            return "♥"
+        case .diamond:
+            return "♦"
+        }
+    }
+}
+
+extension CardData.NumericData: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .two, .three, .four, .five, .six, .seven, .eight, .nine, .ten:
+            return "\(self.rawValue)"
+        case .jack:
+            return "J"
+        case .queen:
+            return "Q"
+        case .king:
+            return "K"
+        }
+    }
 }
