3a7c18ce45ed889c3b357500354255823dd49959
sol <hskim6521@naver.com>
2022-02-22 14:48:32 +0900

    Test: CardDeck 구조체의 함수 테스트 구현

--- PockerGameApp/PockerGameAppTests/PockerGameAppTests.swift
+++ PockerGameApp/PockerGameAppTests/PockerGameAppTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import PockerGameApp
 
 class PockerGameAppTests: XCTestCase {
 
@@ -13,4 +14,32 @@ class PockerGameAppTests: XCTestCase {
         
     }
 
+    func initializeCardDeckTest() {
+        var cardArrayMustBeEqual = [Card]()
+        
+        for numberCase in Card.Number.allCases {
+            for symbolCase in Card.Symbol.allCases {
+                cardArrayMustBeEqual.append(Card(number: numberCase, symbol: symbolCase))
+            }
+        }
+        
+        XCTAssertEqual(CardDeck.init(), CardDeck.init(cardArrayMustBeEqual), "초기화된 값이 일치하지 않습니다.")
+    }
+    
+    func shuffleCardDeckTest() {
+        let testCardDeck1 = CardDeck()
+        var testCardDeck2 = CardDeck()
+        
+        testCardDeck2.shuffle()
+        
+        XCTAssertNotEqual(testCardDeck1, testCardDeck2, "셔플되지 않았습니다.")
+    }
+    
+    func removeCardDeckTest() {
+        var testCardDeck = CardDeck()
+        let previousCardsCount = testCardDeck.count
+        print(testCardDeck.removeOne()!)
+        
+        XCTAssertEqual(testCardDeck.count, previousCardsCount - 1, "한 장이 뽑혀 제거되지 않았습니다.")
+    }
 }
