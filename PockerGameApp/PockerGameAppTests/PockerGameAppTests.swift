b1b3d9d27c07bae6f23096b1c7e63fd1e4ac7b76
sol <hskim6521@naver.com>
2022-02-22 14:37:10 +0900

    Test: Test Target 생성

---
+++ PockerGameApp/PockerGameAppTests/PockerGameAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PockerGameAppTests.swift
+//  PockerGameAppTests
+//
+//  Created by 김한솔 on 2022/02/22.
+//
+
+import XCTest
+
+class PockerGameAppTests: XCTestCase {
+
+    func testScenario() throws {
+        
+    }
+
+}
