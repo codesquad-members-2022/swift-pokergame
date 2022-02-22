244eba9bd81a9437879f94006f32f72ef9e37b9a
P_SeoB <akdlsjtm21@naver.com>
2022-02-22 18:29:09 +0900

    [Create] Card,Rank,Suit

---
+++ PokerGame/PokerGame/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  PokerGame
+//
+//  Created by 박진섭 on 2022/02/22.
+//
+
+import Foundation
+
+//카드를 만들 클래스
+class Card {
+    
+}
---
+++ PokerGame/PokerGame/Rank.swift
@@ -0,0 +1,14 @@
+//
+//  Rank.swift
+//  PokerGame
+//
+//  Created by 박진섭 on 2022/02/22.
+//
+
+import Foundation
+
+//카드의 Rank(숫자)를 나타내줄 Enum
+//카드의 숫자는 1~13으로 일정한 값만이 들어올것이기 때문에 예외처리를 원활하게 하기 위해서 enum으로 만든다.
+enum Rank {
+    
+}
