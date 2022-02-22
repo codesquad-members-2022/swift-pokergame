f16ff3f4260c1d87ec041125ba0789c5b4eee6ec
P_SeoB <akdlsjtm21@naver.com>
2022-02-22 18:29:09 +0900

    [Create] Card,Rank,Suit

---
+++ PokerGame/PokerGame/Suit.swift
@@ -0,0 +1,14 @@
+//
+//  Suit.swift
+//  PokerGame
+//
+//  Created by 박진섭 on 2022/02/22.
+//
+
+import Foundation
+
+//카드의 모양(suit)를 나타내줄 Enum
+//카드의 모양은 총 네가지로 고정되어 있으므로 예외처리를 원활하게 하기 위해서 Enum으로 설정한다.
+enum Suit {
+    
+}
