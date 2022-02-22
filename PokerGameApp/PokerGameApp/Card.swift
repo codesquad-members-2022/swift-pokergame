154dd2787f189ebde9862cca574f875abb524a2b
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-22 15:09:01 +0900

    Card 클래스 내부 주석 제거

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -1,11 +1,7 @@
 import Foundation
 
 class Card: CustomStringConvertible{
-    
-    /*
-     카드 모양은 4가지만 존재하기 때문에 열거형으로 각 케이스를 선언했습니다.
-     굳이 클래스나 구조체로 만들 필요 없이 각 케이스별로 일정한 문자열값을 보여주면 된다고 생각했습니다.
-     */
+
     internal enum Suit: CustomStringConvertible, CaseIterable{
         case clover
         case diamond
@@ -25,12 +21,7 @@ class Card: CustomStringConvertible{
             }
         }
     }
-    
-    /*
-     카드 숫자는 실제 숫자와 출력을 위한 문자의 두 가지 속성을 가진 구조체로 선언했습니다.
-     실제 정수타입의 숫자값과 출력을 위한 문자열값을 모두 가지고 있어야 한다고 생각했고,
-     열거형으로 13가지의 케이스를 모두 선언하기 보다는 구조체의 description 프로퍼티값을 switch를 통해 각기 다른 값을 가지도록 하는 것이 더 효율적이라고 생각했습니다.
-     */
+
     internal enum Number:String, CustomStringConvertible, CaseIterable{
         
         case ace = "A"
