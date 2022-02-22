801359aa009a8298d66c6f16f8a7ad9ec51f0d52
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-22 22:42:28 +0900

    [MODIFY]Programmatically make UI for 7 cards

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,19 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var cards: [UIImageView]!
+    
+    var cards : [UIImageView] = []
     let background = UIImage(named:"bg_pattern.png") ?? UIImage()
-    let cardImage = UIImage(named: "card-back.png") ?? UIImage()
+    let backDesign = UIImage(named: "card-back.png") ?? UIImage()
+    let cardss = UIImageView()
+    let spaceBetweenCards :CGFloat = 8
+    let screenSize: CGRect = UIScreen.main.bounds
+    let topsafeArea: CGFloat = 47
+    var cardWidth : CGFloat {
+        (screenSize.width - (spaceBetweenCards * CGFloat(cards.count) - 1))/CGFloat(cards.count)
+    }
+    var cardHeight : CGFloat {
+        cardWidth * 1.27
+    }
+    let startOffsetX:CGFloat = 4
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage:background)
-        for card in cards {
-            card.image = cardImage
-        }
-        
-        
+        makeCards(num: 7)
+        drawCards(cards)
     }
+    
+    
+    func makeCards(num : Int) {
+        for _ in 0..<num {
+            cards.append(UIImageView(image:backDesign))
+        }
+    }
+    
+    func drawCards(_ cards:[UIImageView]) {
+        
+        for (index,card) in cards.enumerated() {
+            card.frame = CGRect(x: startOffsetX + CGFloat(index) * (cardWidth + spaceBetweenCards), y: topsafeArea, width: cardWidth, height: cardHeight)
+            view.addSubview(card)
+        }
+    }
+    
         
 }
 
