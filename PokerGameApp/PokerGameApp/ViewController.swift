819202b873277f060fe992805996dc45c51b010c
Song TaeHwan <axoghks@gmail.com>
2022-02-22 12:57:34 +0900

    feat: Add UIImageViews with card image

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - Properties
-
+    
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setCardViews(count: 7)
+    }
+    
+    // MARK: - Methods
+    func configureUI() {
         self.setBackgroundImage()
     }
-
-    // MARK: - Methods
+    
     func setBackgroundImage() {
         guard let image = UIImage(named: "bg_pattern") else {
             return
@@ -25,6 +34,22 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = UIColor(patternImage: image)
     }
-
+    
+    func setCardViews(count: Int) {
+        let width = Double(self.view.frame.size.width) / Double(count)
+        
+        for i in 0..<count {
+            guard let cardImage = UIImage(named: "card-back") else {
+                return
+            }
+            
+            let cardImageView = UIImageView(image: cardImage)
+            self.view.addSubview(cardImageView)
+            cardImageView.frame.size = CGSize(width: width - 5, height: width * 1.27)
+            cardImageView.frame.origin.x = cardImageView.frame.width * CGFloat(i) + CGFloat(4 * (i + 1))
+            cardImageView.frame.origin.y = self.view.safeAreaLayoutGuide.layoutFrame.origin.y
+        }
+    }
+    
 }
 
