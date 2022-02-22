fb1a6f2cedfe4d0503312c45c750e594d3916590
sanghee <anshee97@naver.com>
2022-02-22 14:47:06 +0900

    Feat: Add CardNumber enumeration

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -21,3 +21,23 @@ enum CardShape: String {
     case heart
     case club
 }
+
+
+
+// 숫자는 1 - 13개 중에 하나를 가질 수 있어서 처음에는 Int형으로 저장해야하나 고민했습니다.
+// 하지만 enum의 case 이름을 숫자로 선언할 수 없기 때문에 영어로 선언하고, rawValue를 String 타입으로 선언하고, 숫자 1, 11, 12, 13은 다른 rawValue를 가지도록 했습니다.
+enum CardNumber: String {
+    case one = "A"
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case ten
+    case eleven = "J"
+    case twelve = "Q"
+    case thirteen = "K"
+}
