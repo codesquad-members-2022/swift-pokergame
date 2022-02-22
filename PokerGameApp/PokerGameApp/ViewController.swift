a67fe8ee3c47e2cbe31705a01679b05dc2863f92
Jinsujin <sylpid003@gmail.com>
2022-02-22 15:17:55 +0900

    Ref: addCardImages 함수 리펙토링
    1. 카드에 사용되는 이미지는 for문 밖에서 한번만 생성되도록 수정
    2. 함수이름 변경
    3. 함수 내에서 UIView 를 생성하는 로직을 함수로 분리

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -13,32 +13,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         addPattern(to: self.view, pattern: #imageLiteral(resourceName: "bg_pattern"))
-        addCardImages(superView: self.view, count: 7)
+        addCardImageContainerView(superView: self.view, cardCount: 7)
     }
     
     private func addPattern(to view: UIView, pattern: UIImage) {
         view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
     }
 
-    private func addCardImages(superView: UIView, count: Int) {
-        let backgroundSize = CGSize(width: superView.bounds.width * 0.9, height: 100)
-        let backgroundPointX = (superView.bounds.width - backgroundSize.width) / 2
-        let background = UIView(frame: CGRect(origin: CGPoint(x: backgroundPointX, y: 100),
-                                size: backgroundSize))
-        background.backgroundColor = .brown
+    private func addCardImageContainerView(superView: UIView, cardCount: Int) {
+        let background = createChildView(superView, multiply: 0.9)
         superView.addSubview(background)
         
         let spacing: CGFloat = 10
-        let cardWidth = (background.frame.size.width - spacing * CGFloat(count - 1)) / CGFloat(count)
+        let cardWidth = (background.frame.size.width - spacing * CGFloat(cardCount - 1)) / CGFloat(cardCount)
         let cardHeight = cardWidth * 1.27
-        for cardIndex in 0..<count {
-            let cardBack: UIImage = #imageLiteral(resourceName: "card-back")
+        let cardBack: UIImage = #imageLiteral(resourceName: "card-back")
+        for cardIndex in 0..<cardCount {
             let space = CGFloat(cardIndex == 0 ? 0 : spacing * CGFloat(cardIndex))
             let x = CGFloat(cardIndex) * cardWidth + space
             let cardImageView =  UIImageView(frame: CGRect(x: x, y: 0, width: cardWidth, height: cardHeight))
             cardImageView.image = cardBack
             background.addSubview(cardImageView)
         }
+    }
+    
+    private func createChildView(_ superView: UIView, multiply: CGFloat) -> UIView {
+        let backgroundSize = CGSize(width: superView.bounds.width * multiply, height: 100)
+        let backgroundPointX = (superView.bounds.width - backgroundSize.width) / 2
+        return UIView(frame: CGRect(origin: CGPoint(x: backgroundPointX, y: 100),
+                                size: backgroundSize))
     }
 }
 
