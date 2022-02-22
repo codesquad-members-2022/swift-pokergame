b14c3436f7b6b38661c5af8036df340fbb1e5c51
bibi <bibi6666667@kakao.com>
2022-02-22 23:36:54 +0900

    ViewController에 카드 한 장 배치하기

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.init(patternImage: UIImage(named: "bg_pattern") ?? UIImage.init())
         
+        let displayWidth = UIScreen.main.bounds.size.width
+        let displayHeight = UIScreen.main.bounds.size.height
+        let cardWidth = displayWidth / 7
+        let cardHeight = cardWidth * 1.27
+        var cardBackView: UIImageView! = UIImageView(frame: CGRect(x: 10, y: 50, width: cardWidth, height: cardHeight))
+        cardBackView.image = UIImage(named: "card-back")
+        cardBackView.frame(forAlignmentRect: CGRect(x: 100, y: 100, width: cardWidth, height: cardHeight))
+        self.view.addSubview(cardBackView)
+        
+        
     }
 
 
