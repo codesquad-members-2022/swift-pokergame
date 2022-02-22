071b037598355899ca706a26903431d44158267e
sanghee <anshee97@naver.com>
2022-02-22 15:04:32 +0900

    Style: Change CardShape Enumeration's type

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -15,11 +15,12 @@ struct Card {
 
 // 카드 모양은 4종류이기 때문이 아래와 같이 선언했습니다.
 // rawValue의 자료형을 String으로 선언하면 rawValue에 원시값을 일일히 저장하지 않아도 case 이름과 동일한 문자열이 rawValue로 생성되기 때문에 아래와 같이 선언했습니다.
-enum CardShape: String {
-    case spade
-    case diamond
-    case heart
-    case club
+// 그러다가 각 카드 모양을 나타내줘야겠다는 생각에 rawValue를 직접 지정하게 되었고, rawValue에는 하나의 문자만 저장하면 되므로 String이 아닌 Character형으로 rawValue 타입을 변경했습니다.
+enum CardShape: Character {
+    case spade = "♠"
+    case diamond = "◆"
+    case heart = "♥"
+    case club = "♣"
 }
 
 
