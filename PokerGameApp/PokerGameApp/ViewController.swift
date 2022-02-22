d8b2f1a651c5fc2decd567419c0d8c8581be2ad2
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-22 14:31:35 +0900

    [MODIFY]Cards layout using Stack View

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,26 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var cards: [UIImageView]!
-    let spaceBetweenCards :CGFloat = 8
-    let screenSize: CGRect = UIScreen.main.bounds
-    var topSafeArea : CGFloat = 47
-    var cardWidth : CGFloat {
-        (screenSize.width - (spaceBetweenCards * CGFloat(cards.count) - 1))/CGFloat(cards.count)
-    }
-    var cardHeight : CGFloat {
-        cardWidth * 1.27
-    }
-    var startOffsetX:CGFloat = 4
     let background = UIImage(named:"bg_pattern.png") ?? UIImage()
     let cardImage = UIImage(named: "card-back.png") ?? UIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage:background)
-        for (index,card) in cards.enumerated() {
+        for card in cards {
             card.image = cardImage
-            card.frame = CGRect(x: startOffsetX + CGFloat(index) * (cardWidth + spaceBetweenCards), y: topSafeArea, width: cardWidth, height: cardHeight)
         }
+        
+        
     }
         
 }
