887697396e918cbc3a78b2756650422b863b508c
sungju-kim <wnwn0719@gmail.com>
2022-02-22 11:52:08 +0900

    Refactor: enum -> nested enum 으로 변경

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -8,35 +8,35 @@
 import Foundation
 
 class Card {
-    var shape : CardShape
-    var number : CardNumber
-    var string : String {
+    // 카드에는 4개의 모양, 13개의 숫자가 정해져 있어 enum 타입으로 정의
+    enum CardShape : String {
+        case heart = "❤️"
+        case spade = "♠️"
+        case clover = "♣️"
+        case diamond = "♦️"
+    }
+    enum CardNumber : String {
+        case A
+        case two = "2"
+        case three = "3"
+        case four = "4"
+        case five = "5"
+        case six = "6"
+        case seven = "7"
+        case eight = "8"
+        case nine = "9"
+        case ten = "10"
+        case J
+        case Q
+        case K
+    }
+    let shape : CardShape
+    let number : CardNumber
+    var string: String {
         return "\(shape.rawValue)\(number.rawValue)"
     }
     init(shape : CardShape, number: CardNumber) {
         self.shape = shape
         self.number = number
     }
-}
-// 카드에는 4개의 모양, 13개의 숫자가 정해져 있어 enum 타입으로 정의
-enum CardShape : String {
-    case heart = "❤️"
-    case spade = "♠️"
-    case clover = "♣️"
-    case diamond = "♦️"
-}
-enum CardNumber : String {
-    case A
-    case two = "2"
-    case three = "3"
-    case four = "4"
-    case five = "5"
-    case six = "6"
-    case seven = "7"
-    case eight = "8"
-    case nine = "9"
-    case ten = "10"
-    case J
-    case Q
-    case K
 }
