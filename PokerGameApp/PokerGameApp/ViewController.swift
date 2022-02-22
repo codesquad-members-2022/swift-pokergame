e79177599aaaa127ab325bc49e57784078411541
Song TaeHwan <axoghks@gmail.com>
2022-02-22 11:21:04 +0900

    feat: Set background color of root view with pattern image

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
 
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setBackgroundImage()
     }
 
+    // MARK: - Methods
+    func setBackgroundImage() {
+        guard let image = UIImage(named: "bg_pattern") else {
+            return
+        }
+        
+        self.view.backgroundColor = UIColor(patternImage: image)
+    }
 
 }
 
