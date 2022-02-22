3d026c69e5673fe388ed746b580867f14e448dc3
Song TaeHwan <axoghks@gmail.com>
2022-02-22 17:02:03 +0900

    feat: Wrap card image view with UIStackView

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - Properties
+    let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        
+        return stack
+    }()
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -17,14 +23,11 @@ class ViewController: UIViewController {
         self.configureUI()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        self.setCardViews(count: 7)
-    }
-    
     // MARK: - Methods
     func configureUI() {
         self.setBackgroundImage()
+        self.view.setBounds(x: 0, y: -44)
+        self.setCardViews(count: 7)
     }
     
     func setBackgroundImage() {
@@ -45,13 +48,21 @@ class ViewController: UIViewController {
         for i in 0..<count {
             let cardImageView = UIImageView(image: cardImage)
             cardImageView.contentMode = .scaleToFill
-            self.view.addSubview(cardImageView)
             cardImageView.frame.size = CGSize(width: width - 5, height: width * 1.27)
             cardImageView.frame.origin.x = cardImageView.frame.width * CGFloat(i) + CGFloat(4 * (i + 1))
             cardImageView.frame.origin.y = self.view.safeAreaLayoutGuide.layoutFrame.origin.y
-            
+            self.stackView.addSubview(cardImageView)
         }
+        
+        self.view.addSubview(stackView)
     }
+    
     
 }
 
+extension UIView {
+    func setBounds(x: Int, y: Int) {
+        self.bounds.origin.x = CGFloat(x)
+        self.bounds.origin.y = CGFloat(y)
+    }
+}
