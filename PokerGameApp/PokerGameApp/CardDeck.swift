1fdc76fb234577e7d15b5e41cc0f2d2d767fe87d
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-22 15:14:31 +0900

    CardDeck.swift 파일 생성

---
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct CardDeck{
+    
+    var count: Int
+    
+    func shuffle(){
+        
+    }
+    
+    func removeOne(){
+        
+    }
+    
+    func reset(){
+        
+    }
+}
