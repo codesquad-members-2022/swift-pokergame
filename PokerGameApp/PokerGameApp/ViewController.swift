3a271e818906e27813280556028a762dcbea7ea1
Damagucci-Juice <jsr9874@gmail.com>
2022-02-22 10:43:08 +0900

    [Step1] Feat : 코드로 뷰 구현 시도

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var  headerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
+        
+        headerView = UIView()
+        headerView.backgroundColor = .red
+        self.view.addSubview(headerView)
     }
-
-
 }
 
