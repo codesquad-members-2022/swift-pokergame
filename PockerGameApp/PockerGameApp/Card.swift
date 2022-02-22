4f310f850e7b5dd36b62422251d3137a976ca628
BumgeunSong <isp1195@gmail.com>
2022-02-22 12:14:16 +0900

    Fix CardNumber description using rawValue

--- PockerGameApp/PockerGameApp/Card.swift
+++ PockerGameApp/PockerGameApp/Card.swift
@@ -66,18 +66,10 @@ enum CardNumber: Int, CustomStringConvertible {
     var description: String {
         switch self {
         case .ace: return "A"
-        case .two: return "2"
-        case .three: return "3"
-        case .four: return "4"
-        case .five: return "5"
-        case .six: return "6"
-        case .seven: return "7"
-        case .eight: return "8"
-        case .nine: return "9"
-        case .ten: return "10"
         case .jack: return "J"
         case .queen: return "Q"
         case .king: return "K"
+        default: return "\(self.rawValue)"
         }
     }
 }
