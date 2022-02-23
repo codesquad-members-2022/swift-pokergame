f18905ca1dd44d59f23a8da2b459def483531eb8
Jinsujin <sylpid003@gmail.com>
2022-02-23 15:59:42 +0900

    Ref: count 테스트 함수의 예상값을 인자로 받도록 수정

--- PokerGameApp/PokerGameApp/TestCardDeck.swift
+++ PokerGameApp/PokerGameApp/TestCardDeck.swift
@@ -24,13 +24,12 @@ class TestCardDeck {
         print("TestCardDeck::countWhenInit() Success-\(expect)개로 초기화 완료")
     }
     
-    func count() {
+    func count(expect: Int) {
         let count = cardDeck.count()
-        
-        if count != 52 {
-            print("TestCardDeck::Count() Fail-초기화했을때 카드 갯수가 예상값(52)가 아닙니다")
+        if count != expect {
+            print("TestCardDeck::count() Fail-카드 갯수가 예상값(\(expect)가 아닙니다")
             return
         }
-        print("TestCardDeck::Count() Success")
+        print("TestCardDeck::count() Success")
     }
 }
