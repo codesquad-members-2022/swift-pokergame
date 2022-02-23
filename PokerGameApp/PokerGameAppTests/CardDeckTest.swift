219e1ff845d06272fdf265590d3d46a7f04ab6d7
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-23 23:21:17 +0900

    CardDeckTest 클래스 추가

---
+++ PokerGameApp/PokerGameAppTests/CardDeckTest.swift
@@ -0,0 +1,28 @@
+import XCTest
+@testable import PokerGameApp
+
+class CardDeckTest: XCTestCase {
+    
+    private var deck: CardDeck!
+    
+    override func setUp() {
+        self.deck = CardDeck()
+    }
+    
+    func testInitialization(){
+        
+    }
+    
+    func testShuffle(){
+        
+    }
+    
+    func testRemoveOne(){
+        
+    }
+    
+    func testReset(){
+        
+    }
+
+}
