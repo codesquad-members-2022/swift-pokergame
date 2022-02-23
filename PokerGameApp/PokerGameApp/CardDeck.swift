db475afd26f18e160b40da4eac291355e3292e5c
asqw887 <asqw887@gmail.com>
2022-02-23 16:52:54 +0900

    feat - CardDeck 함수 구현

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -9,14 +9,54 @@ import Foundation
 
 struct CardDeck {
     
-    private let deck: [Card] = Array()
+    private var deck: [Card] = Array()
+    
+    
+    init() {
+        makeCardDeck()
+    }
+    
+    mutating func makeCardDeck() {
+        for symbol in Card.Symbol.allCases {
+            for rank in Card.Rank.allCases {
+                deck.append(Card(symbol: symbol, rank: rank))
+            }
+        }
+    }
     
     func count() -> Int {
         return deck.count
     }
     
+    mutating func shuffle() {
+        
+        let length = self.count()
+        
+        for i in 0..<length-1 {
+            let randomIndex = Int.random(in: i..<length)
+              
+            let temp = deck[i]
+            deck[i] = deck[randomIndex]
+            deck[randomIndex] = temp
+            
+        }
+        
+    }
     
+    mutating func removeOne() -> Card {
+        
+        let length = self.count()
+        let randomIndex = Int.random(in: 0..<length)
+        
+        let removedCard = deck.remove(at: randomIndex)
+        return removedCard
+        
+    }
     
+    mutating func reset() {
+        deck.removeAll()
+        makeCardDeck()
+    }
     
     
     
