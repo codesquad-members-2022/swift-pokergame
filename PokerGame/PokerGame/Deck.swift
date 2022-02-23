5cd54ac6c105b6e134003de5e8781b077297e4b3
P_SeoB <akdlsjtm21@naver.com>
2022-02-23 20:24:23 +0900

    [ADD] reset Founction

--- PokerGame/PokerGame/Deck.swift
+++ PokerGame/PokerGame/Deck.swift
@@ -10,8 +10,13 @@ import Foundation
 //카드의 모음인 Deck
 //Deck에서 카드를 뽑거나 shuffle을 한다면 해당 Deck참조를 가지고 있는 모든 객체들의 값이 바뀌어야 하므로 Struct대신 Class로 구현했다.
 class Deck {
+    var description: String {
+        return """
+        총 \(count)카드가 있습니다.
+        """
+    }
     
-    var cards:[Card]
+    private var cards:[Card]               //현제 전체 카드들의 정보는 private로 숨긴다
     var count:Int  { self.cards.count }    //Deck안에 있는 Card들은 함수의 호출에 따라 값이 달라지므로 computedproperty로 정의했다.
     
     //init시 덱을 만들도록 설정봤다.
@@ -19,19 +24,27 @@ class Deck {
         self.cards = Card.makeDeck()
     }
     
+    //MARK: -- 함수들. 함수가 실행될 시 제대로 작동이 되는지 확인을 하기위해 리턴값으로 String문자열을 리턴하도록 해보았다.
+    
     //Knuth Shuffle방법은 각 반복마다 남은 element를 셀 필요가 없으므로 Fisher-Yates shuffle보다 시간복잡도 면에서 더 좋을것 같아 택했다.
-    func shuffle() {
+    func shuffle() -> String{
         for cardIndex in 0..<self.count {
             let randomIndex = Int.random(in: cardIndex..<self.count)   //card의 index범위 내에서 랜덤한 index를 뽑고
             cards.swapAt(cardIndex, randomIndex)                      //Array의 기능중 하나인 Swap을 이용 교체한다.
         }
+        return "\(self.count)의 카드를 섞었습니다."
     }
     
     //랜덤한 카드 하나 빼기
-    func removeOne() -> Card{
+    func removeOne() -> String{
         let randomindex = Int.random(in: 0..<self.count)
-        return cards.remove(at: randomindex)
+        let removedCard = cards.remove(at: randomindex)
+        return "\(removedCard)카드를 하나 뽑았습니다."
     }
     
-    
+    //카드를 리셋시키자.
+    func reset() -> String{
+        self.cards = Card.makeDeck()
+        return "카드를 전체를 초기화 했습니다. 현재 카드 수 \(self.count)"
+    }
 }
