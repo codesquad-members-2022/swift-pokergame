67f2369aeb16409ad2eef9a2ac3a0888027d7d90
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-23 23:52:51 +0900

    CardDeckTest 테스트 메소드 추가
    - testShuffle() 제외한 나머지 모두 구현

--- PokerGameApp/PokerGameAppTests/CardDeckTest.swift
+++ PokerGameApp/PokerGameAppTests/CardDeckTest.swift
@@ -10,19 +10,25 @@ class CardDeckTest: XCTestCase {
     }
     
     func testInitialization(){
-        
+        XCTAssertEqual(deck.count, 52,"number of cards not matches")
     }
     
     func testShuffle(){
-        
+//        deck.shuffle()
+//        let originalDeck = CardDeck()
+//        XCTAssertNotEqual(originalDeck, deck)
     }
     
     func testRemoveOne(){
-        
+        deck.reset()
+        XCTAssertNotNil(deck.removeOne())
+        XCTAssertEqual(deck.removeOne()!.suit.description, "♥︎")
     }
     
     func testReset(){
-        
+        XCTAssertNotNil(deck.removeOne())
+        deck.reset()
+        XCTAssertEqual(deck.count, 52)
     }
 
 }
