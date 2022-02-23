4438e62f27f4c89f85d353af28655b4ef50cabe1
OceanShape <oceanshape700@gmail.com>
2022-02-23 15:06:06 +0900

    refactor: Revise loading image and imageView logic

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -16,13 +16,13 @@ class ViewController: UIViewController {
         
         let screenWidth = UIScreen.main.bounds.width
         let cardWidth = screenWidth / 7
+        let image = UIImage(named: "card-back.png")!
         
         for i in 0..<7 {
             let imageView = UIImageView()
 
-            imageView.backgroundColor = UIColor(patternImage: UIImage(named: "card-back.png")!)
             imageView.contentMode = .scaleToFill
-            imageView.image = UIImage(named: "card-back.png")!
+            imageView.image = image
             imageView.frame = CGRect(x: CGFloat(i) * cardWidth, y: 35, width: cardWidth, height: cardWidth * 1.27)
             
             self.view.addSubview(imageView)
