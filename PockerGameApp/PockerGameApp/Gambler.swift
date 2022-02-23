8d399f13a38b5edd58837872f01829d37a038815
sol <hskim6521@naver.com>
2022-02-23 12:30:42 +0900

    Feat: 게임 참가자를 구현한 Gambler 구조체 선언

---
+++ PockerGameApp/PockerGameApp/Gambler.swift
@@ -0,0 +1,18 @@
+//
+//  Gambler.swift
+//  PockerGameApp
+//
+//  Created by 김한솔 on 2022/02/23.
+//
+
+import Foundation
+
+struct Gambler {
+    
+    let name: String
+    var cardDeck = CardDeck.init(with: [])
+    
+    public mutating func getCard(_ card: Card) {
+        self.cardDeck.add(card)
+    }
+}
