881936004e4cadd62e1fd94a3252665743f8245a
bibi <bibi6666667@kakao.com>
2022-02-23 17:01:21 +0900

    숫자와 문자열을 상수로 선언해 사용

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,16 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let backgroundPatternImgName = "bg_pattern"
+    let cardBackImgName = "card-back"
+    let cardNumber: CGFloat = 7
+    let cardRatioOfHeightToWidth: CGFloat = 1.27
+    let cardMarginFromTop: CGFloat = 50
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.init(patternImage: UIImage(named: "bg_pattern") ?? UIImage.init())
+        self.view.backgroundColor = UIColor.init(patternImage: UIImage(named: backgroundPatternImgName) ?? UIImage.init())
         
         let displayWidth = UIScreen.main.bounds.size.width
-        let cardWidth = displayWidth / 7
-        let cardHeight = cardWidth * 1.27
+        let cardWidth = displayWidth / cardNumber
+        let cardHeight = cardWidth * cardRatioOfHeightToWidth
         for i in 0..<7 {
-            let cardBackView: UIImageView! = UIImageView(image: UIImage(named: "card-back"))
-            cardBackView.frame = CGRect(x: CGFloat( Int(cardWidth) * i), y: 50, width: cardWidth, height: cardHeight)
+            let cardBackView: UIImageView! = UIImageView(image: UIImage(named: cardBackImgName))
+            cardBackView.frame = CGRect(x: CGFloat( Int(cardWidth) * i), y: cardMarginFromTop, width: cardWidth, height: cardHeight)
             self.view.addSubview(cardBackView)
         }
         
