19779253fba8041c11445e0cb090abcbeaea665c
shingha <shingha1124@gmail.com>
2022-02-23 18:27:47 +0900

    [refactor] 플레이어 생성 수정

--- PokerGame/PokerGame/Sources/Model/Player/PokerPlayer.swift
+++ PokerGame/PokerGame/Sources/Model/Player/PokerPlayer.swift
@@ -9,13 +9,4 @@ import Foundation
 
 class PokerPlayer: Player {
     
-    var playerNames = ["shingha", "bibi", "alex", "rosa", "chez",
-                       "ocean", "Jason", "Alex", "dale", "kai",
-                       "jee", "mase", "sol", "ebony", "gucci",
-                       "jed", "beck", "eddy", "selina", "pigBag"]
-    
-    init() {
-        let name = playerNames.filter{ $0.count >= 2 && $0.count <= 5 }.randomElement() ?? "noName"
-        super.init(name: name)
-    }
 }
