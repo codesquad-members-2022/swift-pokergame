78f6178972159aea95ff80e2a088d35068690a55
sungju-kim <wnwn0719@gmail.com>
2022-02-23 16:35:40 +0900

    Feat: PokerGame 구조체 구현, CardDeck struct -> class 변경

--- PokerGameApp/PokerGameApp/Pokergame.swift
+++ PokerGameApp/PokerGameApp/Pokergame.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+struct PokerGame {
+    var cardDeck = CardDeck()
+    var dealer : Dealer
+    var player : [Player] = []
+    let stud : Stud
+    let playerCount : PlayerCount
+    init(stud: Stud, playerCount: PlayerCount) {
+        self.stud = stud
+        self.playerCount = playerCount
+        var playerName = PlayerName()
+        for _ in 0..<playerCount.rawValue {
+            let name = playerName.popName()
+            self.player.append(Player(name: name))
+        }
+        self.dealer = Dealer(cardDeck: cardDeck)
+    }
+    mutating func play() {
+        
+    }
+
+    enum PlayerCount : Int{
+        case one = 1
+        case two
+        case three
+        case four
+    }
+    enum Stud {
+        case sevenCard
+        case fiveCard
+    }
+}
