13e5462ffdbe19fdcea30b483896da7e834e68b7
sanghee <anshee97@naver.com>
2022-02-23 15:05:45 +0900

    Feat: Add Initializer to CardDeck

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 struct CardDeck {
+    private var cards: [Card]
     
+    init() {
+        var cards = [Card]()
+        
+        for shape in Card.Shape.allCases {
+            for number in Card.Number.allCases {
+                cards.append(Card(shape: shape, number: number))
+            }
+        }
+        
+        self.cards = cards
+        print("카드 전체를 초기화했습니다.")
+    }
 }
