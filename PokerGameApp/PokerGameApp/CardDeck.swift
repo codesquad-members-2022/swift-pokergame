7aa366257098c6766a27f3e5bf6e45dda1b22614
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-23 14:44:31 +0900

    [Feat] CardDeck 구조체 구현

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -8,5 +8,43 @@
 import Foundation
 
 struct CardDeck {
+    var cards: [Card] = [] //private?
     
+    private var count: Int {
+        return cards.count
+    }
+    
+    init() {
+        setUpCards()
+    }
+    
+    mutating func setUpCards() {
+        let faces = Card.FaceData.allCases
+        let numerics = Card.NumericData.allCases
+        
+        faces.forEach { faceData in
+            numerics.forEach { numericData in
+                cards.append(Card(faceData: faceData, numericData: numericData))
+            }
+        }
+    }
+    
+    mutating func shuffle() {
+        var maxIndex = count - 1
+        
+        while maxIndex != 0 {
+            let randomIndex = Int.random(in: (0...maxIndex))
+            self.cards.swapAt(randomIndex, maxIndex)
+            maxIndex -= 1
+        }
+    }
+    
+    mutating func removeOne() -> Card {
+        return self.cards.remove(at: Int.random(in: (0..<count)))
+    }
+    
+    mutating func reset() {
+        cards.removeAll()
+        setUpCards()
+    }
 }
