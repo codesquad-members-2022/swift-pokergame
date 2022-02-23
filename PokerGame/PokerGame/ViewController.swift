633b0f6d84c8e44c7964893c65b4ab45ce328a0a
P_SeoB <akdlsjtm21@naver.com>
2022-02-23 20:24:23 +0900

    [ADD] reset Founction

--- PokerGame/PokerGame/ViewController.swift
+++ PokerGame/PokerGame/ViewController.swift
@@ -16,7 +16,13 @@ final class ViewController: UIViewController {
 
         //카드 만들기
        let deck = Deck()
+        deck.count
         deck.shuffle()
+        deck.removeOne()
+        deck.removeOne()
+        deck.reset()
+        
+        
     }
     
     //putCardOnView함수를 몇번 실행할지 정하는 함수
