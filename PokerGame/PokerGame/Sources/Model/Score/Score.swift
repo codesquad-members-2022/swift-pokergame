9737ae9acc5a4db6baba64cb1f80e8ede9444d1d
shingha <shingha1124@gmail.com>
2022-02-23 21:52:00 +0900

    [feature] 점수계산 로직 추가

--- PokerGame/PokerGame/Sources/Model/Score/Score.swift
+++ PokerGame/PokerGame/Sources/Model/Score/Score.swift
@@ -14,12 +14,23 @@ class Score {
             let count = cardByNumber[$0.number] ?? 0
             cardByNumber[$0.number] = count + 1
         }
-        let sortCard = player.cards.sorted(by: {return $0.number.rawValue < $1.number.rawValue })
-        print(sortCard)
         
         if cardByNumber.values.contains(4) {
             return .fourCard
-        } else if cardByNumber.values.contains(3) {
+        }
+        
+        let sortCard = player.cards.sorted(by: {return $0.number.rawValue < $1.number.rawValue })
+        let loopCount = sortCard.count - 5
+        for index in 0...loopCount {
+            let firstNumber = sortCard[index].number.rawValue
+            let resultValue = (firstNumber..<firstNumber+5).reduce(0){$0 + $1}
+            let calculateValue = (index..<index+5).reduce(0){$0 + sortCard[$1].number.rawValue}
+            if resultValue == calculateValue {
+                return .straight
+            }
+        }
+        
+        if cardByNumber.values.contains(3) {
             return .triple
         } else if cardByNumber.values.contains(2) {
             let pairCount = cardByNumber.values.filter{ $0 == 2 }.count
@@ -28,9 +39,6 @@ class Score {
             } else {
                 return .onePair
             }
-        } else {
-            let sortCard = player.cards.sorted(by: {return $0.number.rawValue < $1.number.rawValue })
-            print(sortCard)
         }
         
         return .none
