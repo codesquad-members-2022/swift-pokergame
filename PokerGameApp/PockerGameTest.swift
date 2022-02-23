479f1cdbb28f910886b50d7ad554308156383cec
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-23 23:16:29 +0900

    PockerGameTest 테스트코드 작성

--- PokerGameApp/PockerGameTest.swift
+++ PokerGameApp/PockerGameTest.swift
@@ -2,6 +2,26 @@ import XCTest
 @testable import PokerGameApp
 
 class PockerGameTest: XCTestCase {
-
+    
+    private var pockerGame: PockerGame!
+    
+    override func setUp(){
+        pockerGame = PockerGame(numberOfPlayers: 4, stud: 5)
+        super.setUp()
+    }
+    
+    
+    func testInitialization(){
+        XCTAssertEqual(pockerGame.dealer.name, "딜러", "Dealer Successfully Created")
+        XCTAssertEqual(pockerGame.players.count, 4)
+    }
+    
+    func testCardDistribution(){
+        pockerGame.start()
+        for player in pockerGame.players{
+            XCTAssertEqual(player.cards.count, pockerGame.stud)
+        }
+        XCTAssertEqual(pockerGame.dealer.cards.count, pockerGame.stud)
+    }
 
 }
