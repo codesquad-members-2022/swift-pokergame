a4e4e5d3e0485209f3e2ad0afe7e6e4f2352910c
sol <hskim6521@naver.com>
2022-02-23 12:34:59 +0900

    Feat: 포커게임을 구현할 PokerGame 클래스 선언

---
+++ PockerGameApp/PockerGameApp/PockerGame.swift
@@ -0,0 +1,54 @@
+//
+//  PockerGame.swift
+//  PockerGameApp
+//
+//  Created by 김한솔 on 2022/02/23.
+//
+
+import Foundation
+
+class PokerGame {
+    
+    var dealer: Dealer
+    var gamblers: [Gambler]
+    let gameRule: GameRule
+    
+    init(numberOfGamblers: Int) {
+        var nameArray = ["Sol", "Jee", "Eddy", "Foucault", "Selina", "Jed", "Dale"]
+        var gamblers = [Gambler]()
+        for _ in 0..<numberOfGamblers{
+            let newGambler = Gambler(name: nameArray.remove(at: (0..<nameArray.count).randomElement() ?? 0))
+            gamblers.append(newGambler)
+        }
+        
+        self.dealer = Dealer()
+        self.gamblers = gamblers
+        self.gameRule = .sevenCardStud
+    }
+    
+    func gameScenario() {
+        handOut()
+    }
+    
+    private func handOut() {
+        for _ in 0..<gameRule.numberOfCard {
+            for index in 0..<gamblers.count {
+                gamblers[index].getCard(self.dealer.pickCard()!)
+            }
+            dealer.getCard(self.dealer.pickCard()!)
+        }
+    }
+}
+
+enum GameRule {
+    
+    case sevenCardStud
+    case fiveCardStud
+    
+    var numberOfCard: Int {
+        switch self {
+        case .sevenCardStud: return 7
+        case .fiveCardStud: return 5
+        }
+    }
+}
