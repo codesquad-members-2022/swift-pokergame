a897949f1c788f1986c3287d0b5d6d7d6a549661
Jinsujin <sylpid003@gmail.com>
2022-02-23 15:17:08 +0900

    Test: CardDeck 클래스를 테스트하기 위한 파일 생성, count 메서드 테스트 코드 작성
    - TDD 를 시도해보기 위해 CardDeck 클래스 생성전 테스트 코드를 먼저 작성
    - count 메서드를 실행했을때 예상 값을 출력할 수 있는 테스트 코드를 작성
      - 아직 CardDeck 클래스를 생성하기 전이므로 주석처리

---
+++ PokerGameApp/PokerGameApp/TestCardDeck.swift
@@ -0,0 +1,26 @@
+//
+//  TestCardDeck.swift
+//  PokerGameApp
+//
+//  Created by jsj on 2022/02/23.
+//
+
+import Foundation
+
+class TestCardDeck {
+//    private let cardDeck: CardDeck!
+    
+    init() {
+//        self.CardDeck = CardDeck()
+    }
+    
+    func count() {
+//        let count = cardDeck.count()
+        
+//        if count != 52 {
+//            print("TestCardDeck::Count() Fail-초기화했을때 카드 갯수가 예상값(52)가 아닙니다")
+//            return
+//        }
+//        print("TestCardDeck::Count() Success")
+    }
+}
