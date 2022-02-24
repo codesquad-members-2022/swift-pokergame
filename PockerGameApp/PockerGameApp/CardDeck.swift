3ebc91c4e183523c8e7cb5f35423f4389a18eea4
kimdongjune <jun36817@naver.com>
2022-02-24 19:50:23 +0900

    객체 내부의 테스트코드 삭제

--- PockerGameApp/PockerGameApp/CardDeck.swift
+++ PockerGameApp/PockerGameApp/CardDeck.swift
@@ -10,7 +10,7 @@ struct CardDeck{
     public var count: Int{
         return cards.count
     }
-    private var cards: [PockerCard] = []
+    public private(set) var cards: [PockerCard] = []
     private let cardShapes = PockerCard.Shape.allCases
     private let cardNumbers = PockerCard.Number.allCases
     
@@ -28,38 +28,27 @@ struct CardDeck{
     }
     
     mutating func shuffle(){
-        if count > 1{
-            for i in 0 ..< cards.count - 1 { // 0 ~ n-2
-                let randomIndex = Int.random(in: i ..< cards.count)
-                let temp = cards[i]
-                cards[i] = cards[randomIndex]
-                cards[randomIndex] = temp
-            }
+        guard count > 1 else {
+            return
+        }
+        for i in 0 ..< cards.count - 1 { // 0 ~ n-2
+            let randomIndex = Int.random(in: i ..< cards.count)
+            let temp = cards[i]
+            cards[i] = cards[randomIndex]
+            cards[randomIndex] = temp
         }
     }
     
     mutating func removeOne() -> Result<PockerCard, CardDeckError>{
-        if count > 0{
-            guard let card = cards.last else { return .failure(.cardDeckEmpty) }
-            cards.removeLast()
-            return .success(card)
+        guard count > 0 else {
+            return .failure(.cardDeckEmpty)
         }
-        return .failure(.cardDeckEmpty)
+        return .success(cards.removeLast())
     }
     
     mutating func reset(){
         initCardDeckArray()
         shuffle()
-    }
-    
-    mutating func testShuffle() -> Bool{
-        let deckCards = cards
-        shuffle()
-        let duplicated = zip(cards, deckCards).enumerated().filter() {
-            $1.0.number == $1.1.number && $1.0.shape == $1.1.shape
-        }
-        let duplicatedCount = duplicated.map{$0.offset}.count
-        return deckCards.count > duplicatedCount
     }
 }
 
--- PockerGameApp/PockerGameAppTests/CardDeckTest.swift
+++ PockerGameApp/PockerGameAppTests/CardDeckTest.swift
@@ -23,6 +23,12 @@ class CardDeckTest: XCTestCase {
     }
     
     func testShuffle() throws{
-        XCTAssertEqual(cardDeck.testShuffle(), true)
+        let deckCards = cardDeck.cards
+        cardDeck.shuffle()
+        let duplicated = zip(cardDeck.cards, deckCards).enumerated().filter() {
+            $1.0.number == $1.1.number && $1.0.shape == $1.1.shape
+        }
+        let duplicatedCount = duplicated.map{$0.offset}.count
+        XCTAssertTrue(deckCards.count > duplicatedCount)
     }
 }
