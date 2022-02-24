7d6e9d16bb6f3726b8d0e98ddb16b23a8232d643
TaeKyeongKim <kaikim@Kaiui-MacBookPro.local>
2022-02-25 01:49:11 +0900

    [ADD]Person, Dealer, Player, PokerGame

---
+++ PokerGameApp/PokerGameApp/Models/Dealer.swift
@@ -0,0 +1,28 @@
+//
+//  Dealer.swift
+//  포커게임
+//
+//  Created by Kai Kim on 2022/02/24.
+//
+
+import Foundation
+class Dealer : Person {
+    
+    //게임에서 생성된 덱은 딜러만 관리가능
+    private var deck : Deck
+    
+    init(gameDeck : Deck) {
+        self.deck = gameDeck
+        super.init(name: .Dealer)
+    }
+    
+    func dealCard(to player:Player) throws {
+        if let card = deck.removeOne() {
+            player.getCard(card)
+        }else{
+            throw Errors.insufficientCard
+        }
+    }
+    
+    
+}
