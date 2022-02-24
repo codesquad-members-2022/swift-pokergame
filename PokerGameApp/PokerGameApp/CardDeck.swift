b0d66d14d90c878341f0236d873ead0dabc31448
SangHwi-Back <65931336+SangHwi-Back@users.noreply.github.com>
2022-02-24 17:16:43 +0900

    Create CardDeck struct
    외부에 인터페이스만을 공개하는 CardDeck 구조체를 생성합니다.

---
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -0,0 +1,29 @@
+//
+//  CardDeck.swift
+//  PokerGameApp
+//
+//  Created by 백상휘 on 2022/02/24.
+//
+
+import Foundation
+
+struct CardDeck {
+    
+    private var deck: [Card]
+    
+    func count() -> Int {
+        return 0
+    }
+    
+    func shuffle() {
+        
+    }
+    
+    func removeOne() -> Card? {
+        return nil
+    }
+    
+    func reset() {
+        
+    }
+}
