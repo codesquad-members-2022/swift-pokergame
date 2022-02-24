5465095828495724de27b667eb0f395de99f2344
sanghee <anshee97@naver.com>
2022-02-24 16:00:11 +0900

    Test: Add testCardIsInitialized()

--- PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -2,10 +2,26 @@
 //  PokerGameAppTests.swift
 //  PokerGameAppTests
 //
-//  Created by 안상희 on 2022/02/24.
+//  Created by Selina on 2022/02/24.
 //
 
 import XCTest
+@testable import PokerGameApp
 
 class PokerGameAppTests: XCTestCase {
+    var sut: CardDeck!
+    
+    func testCardIsInitialized() {
+        XCTAssertEqual(sut.count, 52, "Card is Not Initialized")
+    }
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = CardDeck()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
 }
