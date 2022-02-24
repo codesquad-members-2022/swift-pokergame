8cdaa8a68f4aa7cfb403bbffd9985cba3b7a11bf
Song TaeHwan <axoghks@gmail.com>
2022-02-24 14:59:34 +0900

    feat: Confirm Comparable protocol to Card class, Suit and Rank enummeration

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -8,55 +8,88 @@
 import Foundation
 
 class Card {
-    enum Suit: CustomStringConvertible {
-        case heart, diamond, spade, clover
-        
-        var description: String {
-            switch self {
-            case .heart:
-                return "❤️"
-            case .clover:
-                return "🍀"
-            case .diamond:
-                return "♦️"
-            case .spade:
-                return "♠️"
-            }
-        }
+    
+    enum Suit: Int {
+        case clover = 1, diamond, heart, spade
     }
     
-    enum Rank: Int, CustomStringConvertible {
+    enum Rank: Int {
         case ace = 1
         case two, three, four, five, six, seven, eight, nine, ten
         case jack, queen, king
         
-        var description: String {
-            switch self {
-            case .ace:
-                return "A"
-            case .jack:
-                return "J"
-            case .queen:
-                return "Q"
-            case .king:
-                return "K"
-            default:
-                return String(self.rawValue)
-            }
-        }
     }
     
-    var suit: Suit
-    var rank: Rank
-    
-    init(suit: Suit, rank: Rank) {
-        self.suit = suit
+    private let suit: Suit
+    private let rank: Rank
+
+    init(rank: Rank, suit: Suit) {
         self.rank = rank
+        self.suit = suit
     }
+    
 }
 
-extension Card: CustomStringConvertible {
+extension Card: Comparable, CustomStringConvertible {
+    
+    static func < (lhs: Card, rhs: Card) -> Bool {
+        if lhs.rank == lhs.rank {
+            return lhs.suit < rhs.suit
+        }
+        
+        return lhs.rank < rhs.rank
+    }
+    
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return lhs.suit == rhs.suit && lhs.rank == rhs.rank
+    }
+    
     var description: String {
         return "\(self.suit) \(self.rank)"
     }
+    
+}
+
+extension Card.Suit: Comparable, CustomStringConvertible {
+    
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+    
+    var description: String {
+        switch self {
+        case .heart:
+            return "❤️"
+        case .clover:
+            return "🍀"
+        case .diamond:
+            return "♦️"
+        case .spade:
+            return "♠️"
+        }
+    }
+    
+}
+
+extension Card.Rank: Comparable, CustomStringConvertible {
+    
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+    
+    var description: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .jack:
+            return "J"
+        case .queen:
+            return "Q"
+        case .king:
+            return "K"
+        default:
+            return String(self.rawValue)
+        }
+    }
+    
 }
