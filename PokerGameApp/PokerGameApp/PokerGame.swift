f6cbcfb68f3d0a95cfc954a844ebae79d7d10e20
Jinsujin <sylpid003@gmail.com>
2022-02-25 03:54:52 +0900

    Feat: 게임 참가를 생성하는 ParticipantFactory 구현 및 적용

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -17,13 +17,41 @@ class PokerGame {
         self.stud = stud
         self.participantCount = participantCount
         self.gameDealer = Dealer(stud: stud)
-        self.players = []
+        self.players = ParticipantFactory.generateParcipants(count: participantCount)
+    }
+}
+
+enum ParticipantCount: Int {
+    case one = 1
+    case two = 2
+    case three = 3
+    case four = 4
+}
+
+struct ParticipantFactory {
+    static func generateParcipants(count: ParticipantCount) -> [Player] {
+        var parcipants = [Player]()
+        for _ in 0..<count.rawValue {
+            guard let randomName = PersonName.allCases.randomElement() else {
+                continue
+            }
+            let player = Player(name: randomName.rawValue)
+            parcipants.append(player)
+        }
+        return parcipants
     }
     
-    enum ParticipantCount: Int {
-        case one = 1
-        case two = 2
-        case three = 3
-        case four = 4
+    enum PersonName: String,CaseIterable {
+        case shingha = "shingha"
+        case chez = "chex"
+        case jee = "jee"
+        case bibi = "bibi"
+        case ebony = "ebony"
+        case mase = "mase"
+        case pigbag = "pigbag"
+        case joobang = "joobang"
+        case dale = "dale"
+        case alex = "alex"
+        case eddy = "eddy"
     }
 }
