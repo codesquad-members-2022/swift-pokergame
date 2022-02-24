066f5237e59a9954b7ec602285f7603ad2b9c57e
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-24 21:53:46 +0900

    ViewController 내의 사용하지 않는 setInitialImageView 메소드 제거

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackgroundColor()
-        //setInitialImageView(7)
         setPokerGame()
         setStudSelectionControl()
         setPlayerCountSelectionControl()
@@ -110,23 +109,6 @@ class ViewController: UIViewController {
         }
         self.labels = []
     }
-    
-    func setInitialImageView(_ cardsCount: CGFloat){
-        guard let image = UIImage(named: "card-back") else{ return }
-        let cardMargin = CGFloat(10)
-        let cardWidth = (self.view.bounds.width - cardMargin*(cardsCount+1))/cardsCount
-        let cardHeight = cardWidth*CGFloat(1.27)
-        var cardXPosition = cardMargin
-        let cardYPosition = CGFloat(80)
-        
-        for _ in 0..<Int(cardsCount){
-            let imageView: UIImageView = UIImageView(frame: CGRect(x: cardXPosition, y: cardYPosition, width: cardWidth, height: cardHeight))
-            imageView.image = image
-            imageView.contentMode = .scaleAspectFit
-            self.view.addSubview(imageView)
-            cardXPosition += cardMargin+cardWidth
-        }
-        
-    }
+
 }
 
