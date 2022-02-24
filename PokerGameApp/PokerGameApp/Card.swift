4709847b3d953d8c4b68f525809ba3ebaa7dc7ee
bibi <bibi6666667@kakao.com>
2022-02-24 23:03:22 +0900

    Card.Number enum 변수명 수정

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -16,20 +16,20 @@ class Card {
         case diamonds = "♦︎"
     }
     
-    enum Number: String {
-        case one = "A"
-        case two = "2"
-        case three = "3"
-        case four = "4"
-        case five = "5"
-        case six = "6"
-        case seven = "7"
-        case eight = "8"
-        case nine = "9"
-        case ten = "10"
-        case eleven = "J"
-        case twelve = "Q"
-        case thirteen = "K"
+    enum Number: Int {
+        case ace = 1
+        case two = 2
+        case three = 3
+        case four = 4
+        case five = 5
+        case six = 6
+        case seven = 7
+        case eight = 8
+        case nine = 9
+        case ten = 10
+        case jack = 11
+        case queen = 12
+        case king = 13
     }
     
     let shape: Shape
@@ -47,3 +47,21 @@ extension Card: CustomStringConvertible {
     }
 }
 
+extension Card.Number: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .jack:
+            return "J"
+        case .queen:
+            return "Q"
+        case .king:
+            return "K"
+        default:
+            return "\(self)"
+        }
+    }
+    
+    
+}
--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -28,16 +28,12 @@ class ViewController: UIViewController {
             self.view.addSubview(cardBackView)
         }
         
-        let heartQ = Card(shape: .hearts, number: .twelve)
+        let heartQ = Card(shape: .hearts, number: .queen)
         let spade7 = Card(shape: .spades, number: .seven)
         print(heartQ, spade7)
+        
+        print(Deck())
     }
     
-    func printCard(cards: Card...) {
-        for card in cards {
-            card.printInfo()
-        }
-    }
-
 }
 
