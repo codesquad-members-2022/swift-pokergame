2d0be3cd0893df9bf68dc7ec6d8148546fb93a12
LeeJoobang <hii5074@gmail.com>
2022-02-24 15:16:02 +0900

    [Add] 앱 배경화면 반영

--- PokerGameApp/PokerGameApp/TestViewController.swift
+++ PokerGameApp/PokerGameApp/TestViewController.swift
@@ -1,25 +1,21 @@
 //
-//  ViewController.swift
+//  TestViewController.swift
 //  PokerGameApp
 //
-//  Created by Joobang Lee on 2022/02/23.
+//  Created by Joobang Lee on 2022/02/24.
 //
 
 import UIKit
 
-class ViewController: UIViewController{
-       
+class TestViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         backgroundImage()
     }
-
+    
     func backgroundImage(){
         guard let backGround = UIImage(named: "bg_pattern.png") else {return}
         view.backgroundColor = UIColor(patternImage: backGround)
     }
-
 }
-  
-    
-
