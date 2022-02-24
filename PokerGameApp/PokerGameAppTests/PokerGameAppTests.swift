c597aa334a3a89216a6072cb2153ead9f223ca68
Jinsujin <sylpid003@gmail.com>
2022-02-25 00:29:30 +0900

    Env: Unit Test 타겟 추가

---
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -0,0 +1,20 @@
+//
+//  PokerGameAppTests.swift
+//  PokerGameAppTests
+//
+//  Created by jsj on 2022/02/25.
+//
+
+import XCTest
+
+class PokerGameAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+}
