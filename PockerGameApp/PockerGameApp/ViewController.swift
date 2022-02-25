b89f991c71b50b1496ddbe8a4f9c10fa62745603
Jason <ggj00125@gmail.com>
2022-02-26 00:54:00 +0900

    FEAT: 카드 7개 만들기

--- PockerGameApp/PockerGameApp/ViewController.swift
+++ PockerGameApp/PockerGameApp/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let numberOfCard = 7
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -17,9 +18,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setBackground()
-        getScreenWidth()
+        createCardRect()
     }
     
+    // Background 설정
     private func setBackground() {
         guard let patternImage: UIImage = UIImage(named: "bg_pattern.png") else {
             return
@@ -27,11 +29,28 @@ class ViewController: UIViewController {
         self.view.backgroundColor = UIColor(patternImage: patternImage)
     }
     
+    // Screen의 Coordinate Value 찾기
     private func getScreenWidth() -> CGFloat {
         let bounds: CGRect = UIScreen.main.bounds // (0.0, 0.0, 390.0, 844.0)
         return bounds.width
     }
     
+    // 카드 7개 만들기
+    private func createCardRect() {
+        for cardCount in 0...numberOfCard {
+            let imageCard = UIImageView()
+            imageCard.image = UIImage(named: "card-back.png")
+            
+            let cardWidth = getScreenWidth() / CGFloat(numberOfCard)
+            let cardHeight = cardWidth * 1.27
+            let cardCoordinateX = (CGFloat(cardCount) * cardWidth)
+            let cardCoordinateY = CGFloat(50)
+            
+            imageCard.frame = CGRect(x: cardCoordinateX, y: cardCoordinateY, width: cardWidth, height: cardHeight)
+            
+            view.addSubview(imageCard)
+        }
+    }
     
 }
 
