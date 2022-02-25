f7392fddd5e95d7dca3c62b4d5ecfbf7e45692e2
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-02-25 16:52:28 +0900

    Player.cards 프로퍼티 접근제어자 private(set)로 변경
    - Dealer 클래스를 별도의 파일로 분리
    - Dealer.takeOutCard() 메소드를 부모 클래스로 이동

--- PokerGameApp/PokerGameApp/Player.swift
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -5,7 +5,7 @@ class Player: CustomStringConvertible{
     var currentName: String{
         return self.name
     }
-    fileprivate (set) var cards: Cards
+    private (set) var cards: Cards
     var description: String{
         return "\(name) \(cards)"
     }
@@ -17,22 +17,6 @@ class Player: CustomStringConvertible{
     
     func addCard(_ card: Card){
         self.cards.addCard(card)
-    }
-}
-
-class Dealer: Player{
-    
-    init() {
-        super.init(name: "딜러")
-    }
-    
-    func takeOutAllNecessaryCards(deck: CardDeck, count: Int)->CardDeck{
-        var deckCopy = deck
-        for _ in 0..<count{
-            guard let card = deckCopy.removeOne() else { continue }
-            super.addCard(card)
-        }
-        return deckCopy
     }
     
     func takeOutCard()-> Card?{
