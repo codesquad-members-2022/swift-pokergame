950f0ce101bc5e00ae2f1ada3725a7df4fe95c92
sanghee <anshee97@naver.com>
2022-02-25 20:56:41 +0900

    Feat: Add Card Distribution function

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -10,8 +10,8 @@ import Foundation
 struct PokerGame {
     private var dealer: Dealer? // run 하면 Dealer를 지정합니다.
     private var players = [Player]() // run 하면 players를 추가해줍니다.
-    private var totalCards = CardDeck() // 전체 카드
-    private var cardStud: Int = 5 // default는 5카드 스터드
+    static var totalCards = CardDeck() // 전체 카드
+    static var cardStud: Int = 5 // default는 5카드 스터드
     
     
     // 게임을 실행합니다.
@@ -25,7 +25,24 @@ struct PokerGame {
         }
         
         dealer = Dealer()
-        dealer?.distributeCard(to: players)
+        
+        var totalCardCount = PokerGame.totalCards.count
+        PokerGame.totalCards.shuffle()
+        
+        // 배분할 카드 수 < 참여자 + 딜러 수이거나, 전체 카드 수가 0이면 종료
+        while !(totalCardCount < playersCount + 1) && totalCardCount != 0 {
+            guard let distributedCardCount = dealer?.distributeCard(to: &players) else {
+                print("오류")
+                break
+            }
+            totalCardCount -= distributedCardCount
+            
+            if dealer?.cards.count == PokerGame.cardStud { // 카드 5장 분배하면 종료..
+                break
+            }
+        }
+        
+        print("종료합니다,,")
     }
     
     
@@ -47,21 +64,27 @@ struct PokerGame {
     
     
     struct Dealer: Participant {
-        var cards = CardDeck()
+        var cards = [Card]()
         
-        func distributeCard(to player: [Player]) -> Int {
-            return 0
+        // 자신을 포함한 플레이어에게 카드를 나눠주고 나눠준 총 카드 수를 리턴합니다.
+        mutating func distributeCard(to players: inout [Player]) -> Int {
+            cards.append(totalCards.removeOne()!)
+            
+            for player in 0..<players.count {
+                players[player].cards.append(totalCards.removeOne()!)
+            }
+            return cards.count
         }
     }
     
     
     struct Player: Participant {
         var name: String
-        var cards = CardDeck()
+        var cards = [Card]()
     }
 }
 
 
 protocol Participant {
-    var cards: CardDeck { get set } // 딜러, 플레이어는 모두 카드 갖고 있음.
+    var cards: [Card] { get set } // 딜러, 플레이어는 모두 카드 갖고 있음. Card 타입을 배열로 갖고 있음
 }
