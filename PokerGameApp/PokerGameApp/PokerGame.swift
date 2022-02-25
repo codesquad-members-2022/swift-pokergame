5fb84095ec819edbeb2c9608b5bbcae7e4e8d602
sanghee <anshee97@naver.com>
2022-02-25 16:28:53 +0900

    Feat: Add Dealer, Player struct

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -8,15 +8,24 @@
 import Foundation
 
 struct PokerGame {
-    private var players = [String]()
-    private var cardStud: Int = 0
+    private var dealer: Dealer? // run 하면 Dealer를 지정합니다.
+    private var players = [Player]() // run 하면 players를 추가해줍니다.
+    private var totalCards = CardDeck() // 전체 카드
+    private var cardStud: Int = 5 // default는 5카드 스터드
     
     
     // 게임을 실행합니다.
     mutating func run() {
-        self.cardStud = 7
-        let playersCount = 4
-        self.players = makeRandomName(count: playersCount)
+        let playersCount = 4 // 플레이어는 1 ~ 4명 (Int.random(in: 1...4))
+        let playerNames = makeRandomName(count: playersCount) // 참가자 이름 배열 생성
+        
+        // 플레이어 추가
+        for name in playerNames {
+            self.players.append(Player(name: name))
+        }
+        
+        dealer = Dealer()
+        dealer?.distributeCard(to: players)
     }
     
     
@@ -35,4 +44,24 @@ struct PokerGame {
         }
         return players
     }
+    
+    
+    struct Dealer: Participant {
+        var cards = CardDeck()
+        
+        func distributeCard(to player: [Player]) -> Int {
+            return 0
+        }
+    }
+    
+    
+    struct Player: Participant {
+        var name: String
+        var cards = CardDeck()
+    }
+}
+
+
+protocol Participant {
+    var cards: CardDeck { get set } // 딜러, 플레이어는 모두 카드 갖고 있음.
 }
