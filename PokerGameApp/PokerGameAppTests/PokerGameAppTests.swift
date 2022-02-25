ecd150367755ce3b01e353d4b791692d3809d10b
Jinsujin <sylpid003@gmail.com>
2022-02-25 15:32:27 +0900

    Test: PokerGame.prepare() 했을때 참가자수와 카드갯수 테스트

--- PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -10,21 +10,14 @@ import XCTest
 
 class PokerGameAppTests: XCTestCase {
     
-    func testAfterPrepare_isSameCardCountPlayers() {
-        let game = PokerGame(stud: .seven, guestCount: .four)
-        game.prepare()
-        let playersState = game.playersState()
-        
-        var isNotSameCardCount = false
-        for state in playersState {
-            if state.cards.count != 7 {
-                isNotSameCardCount = true
-            }
-        }
-        XCTAssertEqual(playersState.count, 5)
-        XCTAssertFalse(isNotSameCardCount, "게임 준비 후, 모든 참가자의 카드갯수가 7장이다")
+    // MARK:- ParticipantFactory
+    func testParticipantFactory_isCorrectGenerateCount() throws {
+        let guests = ParticipantFactory.generateGuests(count: .four)
+        let expectCount = 4
+        XCTAssertEqual(guests.count, expectCount)
     }
     
+    // MARK:- Dealer
     func testDealer_isOutOfCardDeckWhenStartGame() throws {
         let dealer = Dealer(name: "딜러", stud: .five)
         let team = Team(guestCount: .four, dealer: dealer)
@@ -37,5 +30,20 @@ class PokerGameAppTests: XCTestCase {
         }
     }
     
+    // MARK:- PokerGame
+    func testGame_afterPrepare_isSameCardCountPlayers() {
+        let game = PokerGame(stud: .seven, guestCount: .four)
+        game.prepare()
+        let playersState = game.playersState()
+        
+        var isNotSameCardCount = false
+        for state in playersState {
+            if state.cards.count != 7 {
+                isNotSameCardCount = true
+            }
+        }
+        XCTAssertEqual(playersState.count, 5, "게임 준비 후, 모든 참가자의 수는 방문자와 딜러수의 합이다")
+        XCTAssertFalse(isNotSameCardCount, "게임 준비 후, 모든 참가자의 카드갯수가 7장이다")
+    }
     }
 }
