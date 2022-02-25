453398c58d770dd05563aae45ebddb513028e93c
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-25 12:10:58 +0900

    [Feat] Player 구조체 구현

--- PokerGameApp/PokerGameApp/Player.swift
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol Playable {
+    var name: String { get }
+    var hand: Hand { get set }
+    mutating func receive(card: Card)
+}
+
+struct Player: Playable {
+    
+    private(set) var name: String
+    var hand: Hand
+    
+    init() {
+        self.hand = Hand()
+        
+        // TODO: 랜덤한 이름으로 생성되도록 변경
+        self.name = "RandomName"
+    }
+}
+
+extension Playable {
+    mutating func receive(card: Card) {
+        self.hand.receive(card: card)
+    }
+}
