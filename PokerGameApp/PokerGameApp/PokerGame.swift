91e80a5289b34f7e9233aba0ff5c7a2c2f19db53
sanghyeok-kim <slsnsi1212@gmail.com>
2022-02-25 12:00:19 +0900

    [Feat] PokerGame 구조체 및 enum 타입 구현

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct PokerGame {
+    
+    enum Stud: Int {
+        case five
+        case seven
+        
+        var cardCountForGame: Int {
+            switch self {
+            case .five:
+                return 5
+            case .seven:
+                return 7
+            }
+        }
+    }
+    
+    enum PlayerCount: Int {
+        case one = 1
+        case two
+        case three
+        case four
+        
+        var excludeDealer: Int {
+            return self.rawValue
+        }
+    }
+}
