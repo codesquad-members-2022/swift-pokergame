ab93e3b61b72709355d323168c3428fddf5da922
bibi <bibi6666667@kakao.com>
2022-02-25 11:05:44 +0900

    Deck의 count(), removeOne() 메서드 구현 및 주석 삭제

--- PokerGameApp/PokerGameApp/Deck.swift
+++ PokerGameApp/PokerGameApp/Deck.swift
@@ -17,20 +17,22 @@ struct Deck {
         reset()
     }
     
-    func count() {
-        // 갖고 있는 카드 갯수 반환
+    func count() -> Int {
+        return cardDeck.count
     }
     
     func shuffle() {
         // 전체 카드를 랜덤하게 섞음
     }
     
-    func removeOne() {
-        // 카드 인스턴스 중 하나를 반환하고 목록에서 삭제
+    mutating func removeOne() -> Card? {
+        if cardDeck.isEmpty {
+            return nil
+        }
+        return cardDeck.removeLast()
     }
     
     mutating func reset() {
-        // 처음처럼 모든 카드를 다시 채워넣음
         self.cardDeck = Array<Card>()
         for i in 1...initCardNumber {
             self.cardDeck.append(Card(shape: .spades, number: Card.Number.init(rawValue: i) ?? .ace))
