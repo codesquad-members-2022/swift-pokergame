13abf1255dad6dd74f1358e51bb4e6998f53ebf5
Jason <ggj00125@gmail.com>
2022-02-26 00:29:39 +0900

    FEAT: background image 함수로 분리 및 Screen Width값 확인

--- PockerGameApp/PockerGameApp/ViewController.swift
+++ PockerGameApp/PockerGameApp/ViewController.swift
@@ -16,13 +16,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        setBackground()
+        getScreenWidth()
+    }
+    
+    private func setBackground() {
         guard let patternImage: UIImage = UIImage(named: "bg_pattern.png") else {
             return
         }
         self.view.backgroundColor = UIColor(patternImage: patternImage)
     }
     
-
+    private func getScreenWidth() -> CGFloat {
+        let bounds: CGRect = UIScreen.main.bounds // (0.0, 0.0, 390.0, 844.0)
+        return bounds.width
+    }
+    
+    
 }
 
