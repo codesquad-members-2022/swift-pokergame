a9e86efe408dacea50bee11efb3adff9ecd3e8cf
Song TaeHwan <axoghks@gmail.com>
2022-02-27 23:10:45 +0900

    refactor: Covnert instance method and properties into static one

---
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -0,0 +1,8 @@
+//
+//  Dealer.swift
+//  PockerGame
+//
+//  Created by 송태환 on 2022/02/27.
+//
+
+import Foundation
---
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  Pocker
+//
+//  Created by 송태환 on 2022/02/27.
+//
+
+import Foundation
