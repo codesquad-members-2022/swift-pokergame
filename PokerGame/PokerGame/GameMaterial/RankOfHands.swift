cb94c24b04c13f456ee42302aba8a3146addb930
P_SeoB <akdlsjtm21@naver.com>
2022-02-28 12:07:45 +0900

    [Fix] Player & RankOfHands

--- PokerGame/PokerGame/GameMaterial/RankOfHands.swift
+++ PokerGame/PokerGame/GameMaterial/RankOfHands.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 //족보는 변하지 않고 항상 같은 값을 유지하기 때문에 enum으로 주었다.
-enum RankOfHands {
+enum RankOfHands:String {
     case onePair
     case twoPair
     case triple
--- PokerGame/PokerGame/People/Player.swift
+++ PokerGame/PokerGame/People/Player.swift
@@ -9,14 +9,15 @@
 class Player {
     
     let name:String                                     //이름은 한번 정해지면 바뀌지 않지만, 후에 카드게임에서 승자를 가릴때 이름을 가지고 와야하므로 private가아닌 let으로 선언했습니다.
-    private var rankOfHand:RankOfHands = .highCard
+    private var rankOfHand:RankOfHands = .highCard      //아직까진 받은 패를 족보로 계산하는 로직을 짜지않아 highCard로 값을 고정시켰습니다.
+    
     private(set) var cards:[Card] = []                  //뽑은 카드들은 내부에서만 변경가능하고, 후에 게임 결과를 출력할때 필요하므로 private(set)으로 선언했습니다.
     
     init(name:String) {
         self.name = name
     }
     
-    //패를 확인.
+    //패의 결과를 리턴.
     func checkRankOfHand() -> RankOfHands {
         return self.rankOfHand
     }
