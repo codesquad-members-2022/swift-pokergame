ab69614b1f4e35420ec526d53986d5a8cb968334
Damagucci-Juice <jsr9874@gmail.com>
2022-02-28 21:20:47 +0900

    Add : PokerGame 클래스

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -8,7 +8,31 @@
 import Foundation
 
 class PokerGame {
-    private var cardDeck = CardDeck()
-    private var playerCount: Int = 2
+    private var nameArray: [String] = ["HK", "JK", "Crong", "Honux", "Chloe", "Ivy", "Gucci"]
+    enum Games {
+        case sevenCardStud
+        case fiveCardStud
+    }
+    enum PlayerCount: Int {
+        case two = 2, three, four, five
+    }
     
+    private var cardDeck = CardDeck()
+    var sortOfGame: Games
+//    var playerCount: PlayerCount.RawValue = .random(in: 2...5)
+    var playerCount: PlayerCount.RawValue
+    var players: [Playable]
+    var dealer: Dealer
+    
+    init(sortOfGame: Games, playerCount: PlayerCount.RawValue) {
+        self.sortOfGame = sortOfGame
+        self.playerCount = playerCount
+        for index in 0..<playerCount {
+            if index == 0 {
+                self.dealer = Dealer()
+            } else {
+                self.players.append(Player(name: self.nameArray.remove(at: Int.random(in: 0..<self.nameArray.count))))
+            }
+        }
+    }
 }
