76f0cd07e36630f46de66f8db79021b03d11b849
Damagucci-Juice <jsr9874@gmail.com>
2022-02-28 20:53:54 +0900

    Add : Playable 프로토콜 추가

---
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -0,0 +1,14 @@
+//
+//  Dealer.swift
+//  PokerGameApp
+//
+//  Created by YEONGJIN JANG on 2022/02/28.
+//
+
+import Foundation
+
+class Dealer: Playable {
+    private var name = "dealer"
+    private var cardDeck: [Card] = []
+    private var upCards: [Card] = []
+}
