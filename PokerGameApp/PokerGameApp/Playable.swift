00835c07d484021501a7a3afbf549fa05799bb87
Damagucci-Juice <jsr9874@gmail.com>
2022-02-28 21:35:16 +0900

    Add : Computer 클래스 파일

--- PokerGameApp/PokerGameApp/Playable.swift
+++ PokerGameApp/PokerGameApp/Playable.swift
@@ -9,8 +9,7 @@ import Foundation
 
 protocol Playable {
     //TODO: 프로토콜에선 private 선언을 못하는 이유?
-    func addCard(card: Card)
+    func addCard(deck: CardDeck, round: Int) -> Card
     func showUpCards() -> String
     func openAllCards() -> String
-    func 
 }
