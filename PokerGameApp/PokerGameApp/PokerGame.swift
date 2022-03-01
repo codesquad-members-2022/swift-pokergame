f0fe8d2f1a20265cdfcb6dc12bf2a3bb8c86b732
asqw887 <asqw887@gmail.com>
2022-03-01 15:23:37 +0900

    feature - PokerGame 구현

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+
+class PokerGame {
+    
+    private let stud: Stud
+    private let playerCount: Int
+    private var dealer: Dealer
+    private var players: Players
+
+    enum Stud: Int{
+        case five = 5
+        case seven = 7
+    }
+    
+    init(stud: Stud, playerCount: Int){
+        self.stud = stud
+        self.playerCount = playerCount
+        dealer = Dealer()
+        players = Players(playerCount: playerCount, dealer: dealer)
+    }
+    
+    
+    func start() {
+        while dealer.isDealable(players: players, by: stud.rawValue) {
+            dealer.distributeCard(to: players, by: stud.rawValue)
+        }
+    }
+
+    
+    
+}
