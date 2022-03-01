11a5c93776639a14e8175d45344996bf97493341
Jason <ggj00125@gmail.com>
2022-03-01 15:55:05 +0900

    chore: comment에 맞춰 코드 변경
    VSCode를 사용하여 README 작성 및 CLI환경을 같이 쓰다보니 Xcode에서 수정하지않고 VSCode에서 수정하였더니
    반영되지않으므로 앞으로 조심해야겠다

--- PockerGameApp/PockerGameApp/ViewController.swift
+++ PockerGameApp/PockerGameApp/ViewController.swift
@@ -34,21 +34,25 @@ class ViewController: UIViewController {
         let bounds: CGRect = UIScreen.main.bounds // (0.0, 0.0, 390.0, 844.0)
         return bounds.width
     }
+    private func createCardImage() -> UIImageView {
+        let imageCard = UIImageView()
+        imageCard.image = UIImage(named: "card-back.png")
+        return imageCard
+    }
     
     // 카드 7개 만들기
     private func createCardRect() {
         for cardCount in 0...numberOfCard {
-            let imageCard = UIImageView()
-            imageCard.image = UIImage(named: "card-back.png")
+            let card = createCardImage()
             
             let cardWidth = getScreenWidth() / CGFloat(numberOfCard)
             let cardHeight = cardWidth * 1.27
             let cardCoordinateX = (CGFloat(cardCount) * cardWidth)
             let cardCoordinateY = CGFloat(50)
             
-            imageCard.frame = CGRect(x: cardCoordinateX, y: cardCoordinateY, width: cardWidth, height: cardHeight)
+            card.frame = CGRect(x: cardCoordinateX, y: cardCoordinateY, width: cardWidth, height: cardHeight)
             
-            view.addSubview(imageCard)
+            view.addSubview(card)
         }
     }
     
