855acf990f79c09817a0878272af71fc8e327b80
asqw887 <asqw887@gmail.com>
2022-03-01 15:22:41 +0900

    feature - Player 구현

---
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -0,0 +1,8 @@
+//
+//  Dealer.swift
+//  PokerGameApp
+//
+//  Created by 최예주 on 2022/02/28.
+//
+
+import Foundation
---
+++ PokerGameApp/PokerGameApp/Players.swift
@@ -0,0 +1,13 @@
+//
+//  Playable.swift
+//  PokerGameApp
+//
+//  Created by 최예주 on 2022/02/28.
+//
+
+import Foundation
+
+class Players {
+
+    
+}
