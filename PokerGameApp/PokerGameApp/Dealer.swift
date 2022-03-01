a85fcf2cf9399cb7170d7fcccc38df4832e7d88e
asqw887 <asqw887@gmail.com>
2022-03-01 15:22:59 +0900

    feature - Dealer 구현

--- PokerGameApp/PokerGameApp/Dealer.swift
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class Dealer: Player{
+    
+    private var deck: CardDeck = CardDeck()
+    
+    convenience init(){
+        self.init(name: "Dealer")
+    }
+    
+    func deal() -> Card{
+        return deck.removeOne()
+    }
+    
+    func distributeCard(to players: Players, by stub: Int) {
+        for _ in 0..<stub{
+            for index in 0..<players.count() {
+                players.receive(deal(), at: index)
+            }
+        }
+    }
+    
+    func isDealable(players: Players, by stub: Int) -> Bool {
+        if deck.count >= (players.count() * stub) {
+            return true
+        } else{
+            return false
+        }
+    }
+
+}
