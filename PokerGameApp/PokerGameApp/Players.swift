08506cbfd74e734ad3ee22cf604d55e088b5cfcc
asqw887 <asqw887@gmail.com>
2022-03-01 15:23:17 +0900

    feature - Players 구현

--- PokerGameApp/PokerGameApp/Players.swift
+++ PokerGameApp/PokerGameApp/Players.swift
@@ -8,6 +8,26 @@
 import Foundation
 
 class Players {
+    
+    private var sampleNames: [String] {
+        let names = ["rosa","singha","gucci","kai","jee","eddy"]
+        return names.shuffled()
+    }
+    private(set) var players: [Player] = Array()
 
+    init(playerCount: Int, dealer: Dealer){
+        for index in 0..<playerCount {
+            players.append(Player(name:sampleNames[index]))
+        }
+        players.append(dealer)
+    }
+    
+    func count() -> Int{
+        return players.count
+    }
+    
+    func receive(_ card: Card, at index: Int){
+        players[index].receive(card)
+    }
     
 }
