8c825b97e7c4d0106d76bde7f0c13d1ae5c31873
bibi <bibi6666667@kakao.com>
2022-03-02 16:32:10 +0900

    PokerGameAppTests에 SUT 추가 및 setUpWithError(), tearDownWithError() 작성

--- PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -7,29 +7,23 @@
 
 import XCTest
 
+@testable import PokerGameApp // 기능을 테스트할 모델 가져오기 - 모델의 내부 타입과 메서드에 접근 가능하게 함
+
 class PokerGameAppTests: XCTestCase {
+    
+    var systemUnderTest: PokerGame! // System Under Test
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        systemUnderTest = PokerGame(playerNames: ["A", "B"]) // 테스트할 객체 생성
+        // SUT는 setUpWithError()에서 만들고, tearDownWithError()에서 해제하는 것이 좋다.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        systemUnderTest = nil // 테스트한 객체 릴리즈
+        try super.tearDownWithError()
     }
 
 }
