bb2343d16b2c7c35a459ed829f20cf2aed3cdc05
OceanShape <oceanshape700@gmail.com>
2022-03-04 16:52:49 +0900

    refactor: Add rawValue to Card.Shape

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -11,9 +11,8 @@ class Card {
     let num: Num
     let shape: Shape // Card에서만 사용할 가능성이 높으므로, Nested enum타입으로
 
-    // Shape를 원시 타입으로 만든 이유: 특수 문자를 입력받을 경우, 착오가 발생할 수 있기 때문
-    enum Shape {
-        case spade
+    enum Shape: Int {
+        case spade = 1
         case clover
         case diamond
         case heart
@@ -57,20 +56,20 @@ extension Card: CustomStringConvertible {
         }
         
         let n = num.rawValue
-        var ch: Character
+        var str: String
         switch n {
         case 1:
-            ch = "A"
+            str = "A"
         case 11:
-            ch = "J"
+            str = "J"
         case 12:
-            ch = "Q"
+            str = "Q"
         case 13:
-            ch = "K"
+            str = "K"
         default:
-            ch = Character("\(n)")
+            str = String("\(n)")
         }
-        info.append(ch)
+        info.append(str)
         
         return info
     }
