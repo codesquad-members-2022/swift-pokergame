66d9f586463f3acaba121e1f431d8f4e38a4d1bb
OceanShape <oceanshape700@gmail.com>
2022-03-04 16:21:24 +0900

    feat: Create Deck class

---
+++ PokerGameApp/PokerGameApp/Deck.swift
@@ -0,0 +1,16 @@
+//
+//  Deck.swift
+//  PokerGameApp
+//
+//  Created by 허태양 on 2022/02/25.
+//
+
+import Foundation
+
+class Deck {
+    var currentCount: Int = 52
+    var cards: [Card] = []
+ 
+    init() {}
+    
+}
