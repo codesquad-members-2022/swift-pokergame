752cfe496219fbda9d0dee118b2381bd282504f2
LeeJoobang <hii5074@gmail.com>
2022-03-08 15:54:09 +0900

    [Add] 카드덱 생성

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+struct PokerCardDeck {
+    private var pokercard: [PokerCard] = []
+    private var pokercardDeck: [PokerCard] = []
+    
+    init(){
+        makeCardDeck()
+    }
+    
+    mutating func makeCardDeck() {
+        for suits in PokerCard.Suit.allCases{
+            for ranks in PokerCard.Rank.allCases{
+                self.pokercard.append(PokerCard(suits, ranks))
+            }
+        }
+        self.pokercardDeck = pokercard
+    }
+    func count() -> Int{
+        return pokercardDeck.count
+    }
+        
+}
+
